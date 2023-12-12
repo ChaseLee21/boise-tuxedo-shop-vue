12c4ed852e20dbec7d807f57ec029718ab43ec60
Tiny Penguin <chase.seeberger@gmail.com>
2023-12-12 10:35:15 -0800

    timer for hero carousel

--- src/components/HeroCarouselScroll.vue
+++ src/components/HeroCarouselScroll.vue
@@ -4,9 +4,10 @@
             <img class="object-scale-down h-full w-full"  :src="image.src" :alt="image.alt">
         </div>
     </section> -->
-    <section class="wrapper">
-        <div class="carousel" v-for="image in images">
-            <img class="carousel-image"   :src="image.src" :alt="image.alt">
+    <section class="wrapper" ref="carouselWrapper">
+        <div class="carousel" v-for="(image, index) in images" :key="index">
+            <img class="carousel-image" :src="image.src" :alt="image.alt">
+            <p class="carousel-text">{{image.text}}</p>
         </div>
     </section>
 </template>
@@ -17,19 +18,16 @@
     flex-direction: row;
     overflow-x: scroll;
     scroll-snap-type: x mandatory;
-    -webkit-overflow-scrolling: touch;
-    scroll-behavior: smooth;
-    -ms-overflow-style: none;  /* IE and Edge */
-    scrollbar-width: none;  /* Firefox */
     overflow-y: hidden;
-    margin: 1rem
+    margin: 1rem;
+    scroll-margin: 1rem;
+    scroll-behavior: smooth;
 }
 .wrapper .carousel {
     scroll-snap-align: center;
-    flex: 0 0 100%;
+    flex: 0 0 auto;
     position: relative;
     height: 100vh;
-    min-width: 100vw;
     display: flex;
     flex-flow: row nowrap;
     justify-content: center;
@@ -37,13 +35,27 @@
 }
 .carousel-image {
     height: 100%;
+    width: auto;
+    max-width: 100vw;
     object-fit: cover;
     object-position: center;
-
+}
+.carousel-text {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: 0;
+    padding: 1rem;
+    background-color: rgba(0,0,0,0.5);
+    color: white;
+    font-size: 1.5rem;
+    text-align: center;
 }
 </style>
 
 <script setup>
+import { ref, reactive, onMounted, onUnmounted } from 'vue';
 
 const images = [{
     src: "/src/assets/images/burg-wedding.jpg",
@@ -76,4 +88,20 @@ const images = [{
 }
 
 ]
+const carouselWrapper = ref(null);
+const state = reactive({ currentIndex: 0, intervalId: null });
+
+const scrollToNext = () => {
+    state.currentIndex = (state.currentIndex + 1) % images.length;
+    const newScrollPosition = carouselWrapper.value.clientWidth * state.currentIndex;
+    carouselWrapper.value.scrollLeft = newScrollPosition;
+};
+
+onMounted(() => {
+    state.intervalId = setInterval(scrollToNext, 6000); // Change 3000 to the desired interval in milliseconds
+});
+
+onUnmounted(() => {
+    clearInterval(state.intervalId);
+});
 </script>
