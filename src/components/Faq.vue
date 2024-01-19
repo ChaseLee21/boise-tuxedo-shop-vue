6d7642c66b3551d4cae73a0ac61cfb0886d0c214
Tiny Penguin <chase.seeberger@gmail.com>
2024-01-19 14:39:46 -0800

    cleaning up and comments

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -5,6 +5,7 @@
             <li class="text-2xl text-red-600 font-bold">Q: {{ faq.question }}</li>
             <li class="text-lg">A: {{ faq.answer }}</li>
         </ul>
+        <p class="text-lg ms-4 my-3">Not finding the answer you're looking for? Feel free to <router-link class="text-blue-900 font-bold underline" to="/">contact us</router-link></p>
     </section>
 </template>
 
@@ -22,13 +23,27 @@ async function getFaqs() {
         type: 'GET',
         dataType: 'json',
         success: function(data) {
+            for (let faq of data) {
+                faq = formatFaq(faq);
+            }
             faqs.value = data;
         }
     })
+}
+
+function formatFaq(faq) {
+    faq.question = faq.question.trim();
+    faq.answer = faq.answer.trim();
+    faq.question = faq.question.charAt(0).toUpperCase() + faq.question.slice(1);
+    if(faq.question.charAt(faq.question.length - 1) !== '?') {
+        faq.question += '?';
+    }
+    return faq;
 }
 
 onMounted(() => {
     getFaqs();
 })
 
+// TODO: update router-link to go to contact page
 </script>
