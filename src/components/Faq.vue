3a4ccd072fa2c61e5ea047d713a94329c873a91b
Tiny Penguin <chase.seeberger@gmail.com>
2024-01-19 14:34:52 -0800

    api call for FAQS and displaying in component

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -1,6 +1,10 @@
 <template>
-    <section>
-        <h2 class="text-start text-2xl md:text-3xl lg:text-4xl mx-2 font-bold">FAQ</h2>
+    <section class="mx-2">
+        <h2 class="text-start text-2xl md:text-3xl lg:text-4xl font-bold">FAQ</h2>
+        <ul class="flex flex-col ms-4 my-3" v-for="faq in faqs">
+            <li class="text-2xl text-red-600 font-bold">Q: {{ faq.question }}</li>
+            <li class="text-lg">A: {{ faq.answer }}</li>
+        </ul>
     </section>
 </template>
 
@@ -10,14 +14,21 @@ import jquery from 'jquery';
 
 const $ = jquery;
 
-const faq = ref([]);
+const faqs = ref([]);
 
-function getFaq() {
-    
+async function getFaqs() {
+    await $.ajax({
+        url: 'https://boisetuxedoshop.azurewebsites.net/api/faq',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            faqs.value = data;
+        }
+    })
 }
 
 onMounted(() => {
-    getFaq();
+    getFaqs();
 })
 
 </script>
