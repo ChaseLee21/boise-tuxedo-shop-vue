946f94d8e01fbe95b678f5d79e51e629b6a4a797
Tiny Penguin <chase.seeberger@gmail.com>
2024-01-22 12:03:41 -0800

    added privacy policy route and link to termly

---
+++ src/components/Privacy.vue
@@ -0,0 +1,13 @@
+<template>
+    <div name="termly-embed" data-id="daa297aa-9a40-4820-8d62-0f4dae3aac2e"></div>
+</template>
+
+<script type="text/javascript">
+(function(d, s, id) {
+  var js, tjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://app.termly.io/embed-policy.min.js";
+  tjs.parentNode.insertBefore(js, tjs);
+}(document, 'script', 'termly-jssdk'));
+</script>
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import RentOrBuy from './components/RentOrBuy.vue'
 import Product from './components/Product.vue'
 import Category from './components/Category.vue'
 import Faq from './components/Faq.vue'
+import Privacy from './components/Privacy.vue'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -15,7 +16,8 @@ const router = createRouter({
         { path: '/RentOrBuy', component: RentOrBuy, name: 'RentOrBuy' },
         { path: '/Faq', component: Faq, name: 'Faq' },
         { path: '/Product/:id', component: Product, name: 'Product', props: true },
-        { path: '/Category/:category', component: Category, name: 'Category', props: true }
+        { path: '/Category/:category', component: Category, name: 'Category', props: true },
+        { path: '/Privacy', component: Privacy, name: 'Privacy' },
         // other routes...
     ],
     scrollBehavior(to, from, savedPosition) {
