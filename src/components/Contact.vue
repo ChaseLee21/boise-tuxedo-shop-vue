9f0c245a3dc9cdb98acc3ad265c479e5d3ba2b6c
Tiny Penguin <chase.seeberger@gmail.com>
2024-01-23 12:07:45 -0800

    Adding contact route and component

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,12 @@
 <template>
-    <h2>Contact</h2>
+    <h2 class="font-bold text-2xl mx-4 my-1">Contact Us</h2>
+    <!-- List hours of operations -->
+    <Hours class="text-center"/>
+    <!-- List address  -->
+    <!-- Add phone number that is clickable -->
+    <!-- Add email address that is clickable -->
 </template>
 <script setup>
+import Hours from './Hours.vue'
 //TODO: Add contact component
 </script>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,14 +8,7 @@
                 <li><a href="contact.html" class="hover:text-red-400 transition ease-in-out duration-300">Contact</a></li>            
             </ul>
         </section>
-        <section>
-            <ul class="col-span-1 p-2">
-                <li><p class="font-bold">Hours</p></li>
-                <li>Mon – Fri: 10am – 6pm</li>
-                <li>Saturday: 10am – 5pm</li>
-                <li>Sunday: Closed</li>
-            </ul>
-        </section>
+        <Hours />
         <section>
             <ul class="col-span-1 p-2">
                 <li><p class="font-bold">Our Location</p></li>
@@ -26,8 +19,8 @@
                 <li><button class="bg-red-700 text-white p-2 rounded hover:bg-red-900 transition ease-in-out duration-300">(208)375-9616</button></li>
             </ul>
         </section>
-        <p class="col-span-1 md:col-span-3">© 2023 Boise Tuxedo Shop. All rights reserved.</p>
         <section>
+            <p class="col-span-1 md:col-span-3">© 2023 Boise Tuxedo Shop. All rights reserved.</p>
             <ul class="flex flex-row justify-evenly">
                 <li class="underline"><router-link to="/Privacy">Privacy Policy</router-link></li>
                 <li class="underline"><router-link to="/Terms">Terms of Use</router-link></li>
@@ -37,5 +30,6 @@
     </footer>
 </template>
 <script setup>
+import Hours from './Hours.vue'
 //TODO: fix routes that do not use router-link 
 </script>
