131865e8a43de4ed2febcaa404e6a1eb2996bb46
Tiny Penguin <chase.seeberger@gmail.com>
2024-01-23 09:59:39 -0800

    removed unused aboutUs component and ammending previous commit

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,7 +7,6 @@
 </template>
   
 <script setup>
-import Header from './Header.vue';
 import HeroCarousel from './HeroCarouselScroll.vue';
 import ViewProducts from './ViewProducts.vue';
 import GoogleMap from './GoogleMap.vue';
