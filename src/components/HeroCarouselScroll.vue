813d0f62aedc81e2ddd84880ecd03bc1dec4117f
Tiny Penguin <chase.seeberger@gmail.com>
2024-01-23 14:07:34 -0800

    setting up carousel component to retrieve images from database. still need to implement back end

--- src/components/HeroCarouselScroll.vue
+++ src/components/HeroCarouselScroll.vue
@@ -67,37 +67,7 @@
 <script setup>
 import { ref, reactive, onMounted, onUnmounted } from 'vue';
 
-const images = [{
-    src: "/assets/images/burg-wedding.jpg",
-    alt: "A groom and his groomsmen in burgundy tuxedos.",
-    text: "We have tuxedos and suits for every season."
-},{
-    src: "/assets/images/322_aspen_1.jpg",
-    alt: "A young groom dressed in a grey suit with a striped gray tie. His bride is in her wedding dress leaning next to him.",
-    text: "We specialize in weddings."
-},{
-    src: "/assets/images/black-mens-performance-stretch-suit-3.jpg",
-    alt: "A young man dressed in an all black suit posing next to a railing.",
-    text: "Prom is coming soon."
-},{
-    src: "/assets/images/Ike-Behar-Slim-Black-Parker.jpg",
-    alt: "A young man dressed in an all black tuxedo posing next to a railing.",
-    text: "Tuxedos and suits same day rental."
-},{
-    src: "/assets/images/201_burgundy_empire_tuxedo.jpg",
-    alt: "A man in a burgundy dinner shawl 1 button jacket and his bride posing with him in her wedding dress.",
-    text: "Booking weddings today!"
-},{
-    src: "/assets/images/211_tribeca_cobalt_blue_1.jpg",
-    alt: "An adult couple blue tuxedo with a shiny black lapel and his bride posing with him in her wedding dress.",
-    text: "We can cater to out of town groomsmen / weddings."
-},{
-    src: "/assets/images/IndigoBlueTuxedoShawlBlackLapel.jpg",
-    alt: "An adult man in a blue tuxedo with a shiny black lapel posing.",
-    text: "Family owned and operated in the heart of Meridian, Idaho."
-}
-
-]
+const images = []
 const carouselWrapper = ref(null);
 const state = reactive({ currentIndex: 0, intervalId: null });
 
@@ -108,12 +78,17 @@ const scrollToNext = () => {
 };
 
 onMounted(() => {
+    getImages();
     state.intervalId = setInterval(scrollToNext, 8000); // Change 3000 to the desired interval in milliseconds
 });
 
 onUnmounted(() => {
     clearInterval(state.intervalId);
 });
+
+const getImages = () => {
+    //TODO: retrieve images from database
+}
 
 //TODO: along with the non-scrolling carousel, retrieve the images from the database
 //TODO: implement route on backend to store / retrieve carousel images in a table
