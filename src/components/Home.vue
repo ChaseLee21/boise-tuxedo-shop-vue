02d0071d17d2d786c78719492c8301b5657357c9
Tiny Penguin <chase.seeberger@gmail.com>
2024-01-31 15:05:38 -0800

    changes to both carousels and how they retrieve images from api

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <HeroCarouselMobile v-if="isMobile()" :carouselImages = images />
-        <HeroCarousel v-else :carouselImages = images />
+        <HeroCarouselMobile v-if="isMobile" />
+        <HeroCarousel v-else />
         <ViewProducts />
         <GoogleMap />
     </div>
@@ -17,39 +17,8 @@ import { ref, onMounted } from 'vue';
 
 import $ from 'jquery';
 
-// carousel images served to component
-let images = ref([]);
-
-onMounted(() => {
-    getImages();
-});
-
 // USE: this method to determine if the user is on a mobile device
-const isMobile = () => {
-    return window.innerWidth < 768;
-}
+let isMobile = ref(window.innerWidth < 768);
 
-// USE: this method to retrieve images from the carousel api route
-const getImages = () => {
-    $.ajax({
-        url: 'https://boisetuxedoshop.azurewebsites.net/api/carousel',
-        type: 'GET',
-        success: function (data) {
-            validateImages(data);
-        },
-        error: function (error) {
-            console.log(error);
-        }
-    })
-}
 
-// PARAMETERS: data - array of images and their alt text retrieved from the carousel api route
-// USE: this method to validate images retrieved from the carousel api route before displaying them to the user
-const validateImages = (data) => {
-    for (let image of data) {
-        let valid = true;
-        if (!image.url || ! image.imageAlt || image.url == 'string') valid = false;
-        if (valid) images.value.push({ src: image.url, alt: image.imageAlt});
-    }
-}
 </script>
--- src/components/carousel/HeroCarousel.vue
+++ src/components/carousel/HeroCarousel.vue
@@ -1,32 +1,59 @@
 <template>
-    <section class="wrapper relative flex flex-row overflow-scroll scroll-smooth snap-x snap-mandatory m-0 scroll-m-[1rem]">
-        <!-- Previous Button -->
-        <button class="absolute left-0 top-1/2">
-            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" class="stroke-black stroke-2">
-                    <line x1="10" y1="25" x2="40" y2="10"></line>
-                    <line x1="10" y1="25" x2="40" y2="40"></line> 
-            </svg>
-        </button>
+    <section class="wrapper flex flex-row overflow-scroll scroll-smooth snap-x snap-mandatory m-0 scroll-m-[1rem]">
         <!-- Carousel -->
-        <div class="carousel snap-center flex justify-center items-center flex-row flex-nowrap flex-grow-0 flex-shrink-0 flex-auto relative h-auto w-[100vw] " v-for="(image, index) in props.carouselImages" :key="index">
-            <img class="carousel-image h-[100%] w-auto max-w-[100vw] object-cover object-center" :src="image.src" :alt="image.alt">
+        <div class="carousel snap-center flex justify-center items-center flex-row flex-nowrap flex-grow-0 flex-shrink-0 flex-auto relative h-auto w-[100vw] " >
+            <!-- Previous Button -->
+            <button class="absolute left-0 top-1/2" @click="next()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" class="stroke-black stroke-2">
+                        <line x1="10" y1="25" x2="40" y2="10"></line>
+                        <line x1="10" y1="25" x2="40" y2="40"></line> 
+                </svg>
+            </button>
+            <img class="carousel-image h-[100%] w-auto max-w-[100vw] object-cover object-center" v-if="currentImage" :src="currentImage.src">
+            <!-- Next Button -->
+            <button class="absolute top-1/2 right-0" @click="previous">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" class="stroke-black stroke-2">
+                        <line x1="40" y1="25" x2="10" y2="10"></line>
+                        <line x1="40" y1="25" x2="10" y2="40"></line> 
+                </svg>
+            </button>
         </div>
-        <!-- Next Button -->
-        <button class="absolute top-1/2 right-0">
-            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" class="stroke-black stroke-2">
-                    <line x1="40" y1="25" x2="10" y2="10"></line>
-                    <line x1="40" y1="25" x2="10" y2="40"></line> 
-            </svg>
-        </button>
     </section>
 </template>
 
 <script setup>
-const props = defineProps({
-    carouselImages: {
-        type: Array,
-        required: true
-    }
+import { ref, onMounted } from 'vue';
+import $ from 'jquery';
+
+let images = ref([]);
+let carouselIndex = ref(0);
+let currentImage = ref(images.value[carouselIndex.value]); // Initialize currentImage as null
+
+const next = () => {
+    carouselIndex.value = (carouselIndex.value + 1) % images.value.length;
+    currentImage.value = images.value[carouselIndex.value];
+};
+
+const previous = () => {
+    carouselIndex.value = (carouselIndex.value - 1 + images.value.length) % images.value.length;
+    currentImage.value = images.value[carouselIndex.value];
+}; 
+
+// USE: this method to retrieve images from the carousel api route
+onMounted(async () => {
+    await $.ajax({
+        url: 'https://boisetuxedoshop.azurewebsites.net/api/carousel',
+        type: 'GET',
+        error: function (error) {
+            console.log(error);
+        },
+        success: function (data) {
+            images.value = data;
+            currentImage.value = images.value[carouselIndex.value];
+        }
+    });
+    console.log(images.value);
+    console.log(currentImage);
 });
 
 </script>
