ea65e55afe21575ba3596ccb9ceba158b1bda7e7
Tiny Penguin <chase.seeberger@gmail.com>
2024-01-31 10:40:58 -0800

    validating images retrieved from api and comments

--- src/components/HeroCarouselScroll.vue
+++ src/components/HeroCarouselScroll.vue
@@ -6,8 +6,7 @@
     </section> -->
     <section class="wrapper" ref="carouselWrapper">
         <div class="carousel" v-for="(image, index) in images" :key="index">
-            <img class="carousel-image" :src="image.src" :alt="image.alt">
-            <p class="carousel-text">{{image.text}}</p>
+            <img class="carousel-image" :src="image.url" :alt="image.imageAlt">
         </div>
     </section>
 </template>
@@ -87,25 +86,28 @@ onUnmounted(() => {
     clearInterval(state.intervalId);
 });
 
+// USE: this method to retrieve images from the carousel api route
 const getImages = () => {
-    //TODO: retrieve images from database
-    $.ajax(
-        {
-            url: 'https://boisetuxedoshop.azurewebsites.net/api/carousel',
-            type: 'GET',
-            success: function (data) {
-                validateImages(data);
-            },
-            error: function (error) {
-                console.log(error);
-            }
+    $.ajax({
+        url: 'https://boisetuxedoshop.azurewebsites.net/api/carousel',
+        type: 'GET',
+        success: function (data) {
+            validateImages(data);
+        },
+        error: function (error) {
+            console.log(error);
         }
-    )
+    })
 }
 
-const validateImages = (images) => {
-    images.value = images;
-    console.log(images.value);
+// PARAMETERS: data - array of images and their alt text retrieved from the carousel api route
+// USE: this method to validate images retrieved from the carousel api route before displaying them to the user
+const validateImages = (data) => {
+    for (let image of data) {
+        let valid = true;
+        if (!image.url || ! image.imageAlt || image.url == 'string') valid = false;
+        if (valid) images.push({ src: image.url, alt: image.alt});
+    }
 }
 
 //TODO: along with the non-scrolling carousel, retrieve the images from the database
