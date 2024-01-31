e5a3165393201b4c18df7fad05809b8ac9c2af83
Tiny Penguin <chase.seeberger@gmail.com>
2024-01-31 11:01:29 -0800

    serving different carousel components based on devices size

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,19 @@
 <template>
     <div>
-        <HeroCarousel />
+        <HeroCarouselMobile v-if="isMobile()"/>
+        <HeroCarousel v-else />
         <ViewProducts />
         <GoogleMap />
     </div>
 </template>
   
 <script setup>
-import HeroCarousel from './carousel/HeroCarouselMobile.vue';
+import HeroCarouselMobile from './carousel/HeroCarouselMobile.vue';
+import HeroCarousel from './carousel/HeroCarousel.vue';
 import ViewProducts from './ViewProducts.vue';
 import GoogleMap from './GoogleMap.vue';
 
-//TODO: serve correct herocarousel based on screen size
+const isMobile = () => {
+    return window.innerWidth < 768;
+}
 </script>
