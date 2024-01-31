bb6b9cc933723cea68e93dd826c8c88d9c6277f6
Tiny Penguin <chase.seeberger@gmail.com>
2024-01-31 11:08:28 -0800

    inserted base for desktop carousel copied from mobile

--- src/components/carousel/HeroCarousel.vue
+++ src/components/carousel/HeroCarousel.vue
@@ -1,5 +1,9 @@
 <template>
-
+    <section class="wrapper flex flex-row overflow-scroll scroll-smooth snap-x snap-mandatory m-0 scroll-m-[1rem]">
+        <div class="carousel snap-center flex justify-center items-center flex-row flex-nowrap flex-grow-0 flex-shrink-0 flex-auto relative h-auto w-[100vw] " v-for="(image, index) in props.carouselImages" :key="index">
+            <img class="carousel-image h-[100%] w-auto max-w-[100vw] object-cover object-center" :src="image.src" :alt="image.alt">
+        </div>
+    </section>
 </template>
 
 <script setup>
