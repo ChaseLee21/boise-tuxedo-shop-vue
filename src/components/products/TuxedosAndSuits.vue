302554b9f0da3b0789b4e8268a603ffcd24c4130
Tiny Penguin <chase.seeberger@gmail.com>
2024-02-01 15:22:33 -0800

    added buttons to tuxedos and suits component to make desktop friendly

--- src/components/products/TuxedosAndSuits.vue
+++ src/components/products/TuxedosAndSuits.vue
@@ -7,10 +7,17 @@
             <router-link class="text-base underline button-class" :to="{ name: 'Category', params: {category : 'TuxedoSuit' } }">View All</router-link>
         </div>
         <!-- Product Container -->
-        <div class="flex flex-row flex-nowrap overflow-scroll scroll-smooth snap-x snap-mandatory m-0 scroll-m-[1rem]">
+        <div ref="productContainer" class="relative flex flex-row flex-nowrap overflow-scroll scroll-smooth snap-x snap-mandatory m-0 scroll-m-[1rem] md:overflow-hidden">
+            <!-- Previous Button -->
+            <button class="sticky left-0 top-1/2" @click="scroll(-1)">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" class="stroke-black stroke-2 opacity-50 hover:opacity-75">
+                        <line x1="10" y1="25" x2="40" y2="10"></line>
+                        <line x1="10" y1="25" x2="40" y2="40"></line> 
+                </svg>
+            </button>
             <!-- Product Card -->
             <div class="flex flex-col snap-center text-center min-w-[90%] md:min-w-[56%] lg:min-w-[28%] mx-2 md:mx-4 my-2 bg-gray-800 text-white rounded shadow-lg shadow-black" v-for="product in props.tuxedoAndSuitProducts">
-                <h3 class="text-2xl">{{ product.name }}</h3>
+                <h3 class="text-xl md:text-2xl p-1">{{ product.name }}</h3>
                 <!-- Product Image -->
                 <div class="flex justify-center w-full my-1 md:my-2 p-2">
                     <img class="h-[40rem] max-h-[75vh] w-fit object-cover object-center rounded shadow shadow-black" :src="product.imageURL" :alt="product.imageAlt" >
@@ -20,6 +27,13 @@
                 </div>
                 <p class="my-1 smd:my-2">Style Number: {{ product.styleNumber }}</p>
             </div>
+            <!-- Next Button -->
+            <button class="sticky top-1/2 right-0" @click="scroll(1)">
+                <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" class="stroke-black stroke-2 opacity-50 hover:opacity-75">
+                        <line x1="40" y1="25" x2="10" y2="10"></line>
+                        <line x1="40" y1="25" x2="10" y2="40"></line> 
+                </svg>
+            </button>
         </div>
 
     </section>
@@ -29,10 +43,20 @@
 </style>
 
 <script setup>
+import { ref, onMounted } from 'vue';
+
 const props = defineProps({
     tuxedoAndSuitProducts: {
         type: Array,
         required: true
     }
 })
+
+const productContainer = ref(null);
+
+const scroll = (direction) => {
+    const container = productContainer.value;
+    const scrollAmount = direction * container.clientWidth;
+    container.scrollLeft += scrollAmount;
+}
 </script>
