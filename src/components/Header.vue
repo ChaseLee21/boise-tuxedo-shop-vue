965dd80dea1e64a7f3ef140c20bfe55e9bba91ee
Tiny Penguin <chase.seeberger@gmail.com>
2024-01-31 16:21:58 -0800

    updating the header because it was really empty

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,28 @@
 <template>
-    <header class="flex flex-row">
-        <section class="m-auto">
+    <header class="grid grid-cols-3">
+        <section class="col-span-1 m-auto">
+            <ul class="flex flex-col text-center justify-center m-auto text-lg font-bold">
+                <li class="text-red-600">All Tuxedos and Suits in Stock</li>
+                <li>5 Star Customer Services</li>
+                <li>No Appointment Necessary</li>
+                <li>Its Never to Late to Shop for your Prom or Wedding Attire</li>
+            </ul>
+        </section>
+        <section class="col-span-1 flex flex-row justify-center">
             <img src="/src/assets/images/Tuxedo-Shop-Logo-3.jpg" alt="Logo including a top hat, part of a suit, and the words Boise Tuxedo Shop." >
+        </section>
+        <section class="col-span-1 m-auto">
+            <div class="flex justify-center">
+                <a href="https://www.facebook.com/your-facebook-page" target="_blank" rel="noopener noreferrer">
+                    <font-awesome-icon icon="facebook" />
+                </a>
+                <a href="https://www.instagram.com/your-instagram-page" target="_blank" rel="noopener noreferrer">
+                    <font-awesome-icon icon="instagram" />
+                </a>
+            </div>
         </section>
     </header>
 </template>
+
 <script setup>
 </script>
