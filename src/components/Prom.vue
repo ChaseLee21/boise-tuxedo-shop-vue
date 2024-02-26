6627f3aaeedd88d9f9eb8ffd80514a1a74b76fd1
Tiny Penguin <chase.seeberger@gmail.com>
2024-02-26 14:12:08 -0800

    created prom view and route

---
+++ src/components/Prom.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Prom</h1>
+</template>
--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import Privacy from './components/Privacy.vue'
 import Terms from './components/Terms.vue'
 import Disclaimer from './components/Disclaimer.vue'
 import Contact from './components/Contact.vue'
+import Prom from './components/Prom.vue'
 
 const router = createRouter({
     history: createWebHistory(),
@@ -27,6 +28,7 @@ const router = createRouter({
         { path: '/Terms', component: Terms, name: 'Terms' },
         { path: '/Disclaimer', component: Disclaimer, name: 'Disclaimer' },
         { path: '/Contact', component: Contact, name: 'Contact' },
+        { path: '/Prom', component: Prom, name: 'Prom' },
         // other routes...
     ],
     scrollBehavior(to, from, savedPosition) {
