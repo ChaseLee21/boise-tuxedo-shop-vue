3e074371810d0b65aa538626662f3c4870c57da9
Tiny Penguin <chase.seeberger@gmail.com>
2024-02-27 12:01:21 -0800

    updated faq component to use fetch request instead of jquery ajax request, and updated html to follow semantic guidelines

--- src/components/Faq.vue
+++ src/components/Faq.vue
@@ -1,48 +1,32 @@
 <template>
-    <section class="mx-2 text-start xl:w-[80vw] xl:flex xl:flex-col xl:m-auto">
-        <h1 class="text-start underline text-2xl md:text-3xl lg:text-4xl font-bold">FAQ</h1>
-        <ul class="flex flex-col ms-4 my-3" v-for="faq in faqs">
-            <li class="text-2xl text-red-600 font-bold">{{ faq.question }}</li>
-            <li class="text-lg">{{ faq.answer }}</li>
-        </ul>
-        <p class="text-lg ms-4 my-3">Not finding the answer you're looking for? Feel free to <router-link class="text-blue-900 font-bold underline" to="/Contact">contact us</router-link></p>
-    </section>
+    <main class="xl:w-[80vw] xl:flex xl:flex-col xl:m-auto">
+        <header class="mb-2">
+            <h1 class="text-2xl md:text-3xl lg:text-4xl font-bold underline py-2">FAQs</h1>
+            <p class="text-lg">Not finding the answer you're looking for? Feel free to <router-link class="text-blue-900 font-bold underline" to="/Contact">contact us</router-link></p>
+        </header>
+        <section role="contentinfo">
+            <article v-for="faq in faqs">
+                <h2 class="text-2xl text-red-600 font-bold">{{ faq.question }}</h2>
+                <p class="indent-3">{{ faq.answer }}</p>
+            </article>
+        </section>
+    </main>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import jquery from 'jquery';
-
-const $ = jquery;
 
 const faqs = ref([]);
 
 async function getFaqs() {
-    await $.ajax({
-        url: 'https://boisetuxedoshop.azurewebsites.net/api/faq',
-        type: 'GET',
-        dataType: 'json',
-        success: function(data) {
-            for (let faq of data) {
-                faq = formatFaq(faq);
-            }
-            faqs.value = data;
-        }
-    })
-}
-
-function formatFaq(faq) {
-    faq.question = faq.question.trim();
-    faq.answer = faq.answer.trim();
-    faq.question = faq.question.charAt(0).toUpperCase() + faq.question.slice(1);
-    if(faq.question.charAt(faq.question.length - 1) !== '?') {
-        faq.question += '?';
+    const response = await fetch('https://boisetuxedoshop.azurewebsites.net/api/faq');
+    if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
     }
-    return faq;
+    faqs.value = await response.json();
 }
 
 onMounted(() => {
     getFaqs();
 })
-
 </script>
