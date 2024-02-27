574da209966d584b2857997e56b2516ea7ce6507
Tiny Penguin <chase.seeberger@gmail.com>
2024-02-27 11:26:38 -0800

    increased margin for the contact view on larger screens

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,12 +1,14 @@
 <template>
-    <section class="flex flex-col text-start justify-center p-2 md:text-center ">
-        <h2 class="font-bold text-2xl my-1">Contact Us</h2>
-        <ul class="my-1 mx-1 list-disc list-inside md:list-none">
-            <li>Please refer to the <router-link class="text-blue-900 font-bold underline" to="/Faq">FAQ's</router-link> before calling to see if your question is already answered!</li>
-            <li>If you are a part of a wedding party and are ready to give us your measurements, please send us an email with your name, wedding date, groom's name, and your measurements.</li>
-            <li>If you still have questions or concerns, please call us or send us an email and we will get back to you as soon as possible!</li>
-            <li><a class="text-blue-900 font-bold underline" href="tel:208-375-9616" alt="208-375-9616">Call: (208) 375-9616</a></li>
-            <li><a class="text-blue-900 font-bold underline" href="mailto:boisetuxedoshop@gmail.com" target="_blank">Email: boisetuxedoshop@gmail.com</a></li>
-        </ul>
-    </section>
+    <div class="xl:w-[80vw] xl:flex xl:flex-col xl:m-auto">
+        <section class="flex flex-col text-start justify-center p-2 ">
+            <h1 class="font-bold text-2xl my-1">Contact Us</h1>
+                <ul class="my-1 mx-1 list-disc list-inside md:list-none">
+                    <li>Please refer to the <router-link class="text-blue-900 font-bold underline" to="/Faq">FAQ's</router-link> before calling to see if your question is already answered!</li>
+                    <li>If you are a part of a wedding party and are ready to give us your measurements, please send us an email with your name, wedding date, groom's name, and your measurements.</li>
+                    <li>If you still have questions or concerns, please call us or send us an email and we will get back to you as soon as possible!</li>
+                    <li><a class="text-blue-900 font-bold underline" href="tel:208-375-9616" alt="208-375-9616">Call: (208) 375-9616</a></li>
+                    <li><a class="text-blue-900 font-bold underline" href="mailto:boisetuxedoshop@gmail.com" target="_blank">Email: boisetuxedoshop@gmail.com</a></li>
+                </ul>
+            </section>
+    </div>
 </template>
