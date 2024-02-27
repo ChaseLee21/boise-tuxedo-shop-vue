5e9989ea75209fb7ae46b0500f8748cb01367c64
Tiny Penguin <chase.seeberger@gmail.com>
2024-02-27 10:46:43 -0800

    added a FeaturedProducts for prom in the Prom vue

--- src/components/Prom.vue
+++ src/components/Prom.vue
@@ -57,4 +57,50 @@
             <li>Placeholder</li>
         </ul>
     </section>
+    <FeaturedProducts :products = promProducts :title = prom.title :route = prom.route />
+
 </template>
+
+<script setup>
+import FeaturedProducts from './FeaturedProducts.vue';
+import jQuery from 'jquery';
+import { computed, onMounted, ref } from 'vue';
+
+const $ = jQuery;
+
+const prom = {
+    title: "Popular Tuxedos & Suits for Prom 2024",
+    route: "Prom"
+} 
+
+const products = ref([]);
+
+onMounted(() => {
+    getProducts();
+})
+
+async function getProducts() {
+    await $.ajax({
+        url: 'https://boisetuxedoshop.azurewebsites.net/api/products',
+        type: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            for (let product of data) {
+                if (product.keyFeatures) {
+                    product.keyFeatures = product.keyFeatures.split(',');
+                }
+            }
+            products.value = data;
+        }
+    })
+}
+
+const promProducts = computed(() => {
+    let arr = products.value.filter(product => product.type === "Prom");
+    while (arr.length > 6) {
+        arr.pop();
+    }
+    return arr;
+})
+
+</script>
