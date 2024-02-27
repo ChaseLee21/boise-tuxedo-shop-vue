94a078eb4dd1ee7250e99f5b04c5d9ee640cc5d7
Tiny Penguin <chase.seeberger@gmail.com>
2024-02-27 11:20:55 -0800

    changed text size and bg color of the shop experience component

--- src/components/ViewProducts.vue
+++ src/components/ViewProducts.vue
@@ -1,13 +1,13 @@
 <template>
-    <section class="bg-zinc-700 text-white p-10   text-center">
+    <section class="bg-gray-800 text-white p-10   text-center">
             <p class="text-3xl md:text-4xl lg:text-5xl font-bold">The Boise Tuxedo Shop Experience:</p>
             <br class="line-through" />
-            <ul class="flex flex-row overflow-x-auto md:grid md:grid-flow-row md:grid-cols-2 lg:grid-cols-4 text-2xl md:text-3xl lg:text-4xl">
+            <ul class="flex flex-row overflow-x-auto md:grid md:grid-flow-row md:grid-cols-2 lg:grid-cols-4 ">
                 <li class="bg-white border rounded m-2 p-4 md:row-span-1 md:hover:drop-shadow-xl" v-for="specialty in specialties" :key="specialty.id">
                     <div class="w-52 md:w-auto ">
-                        <p class="text-red-600 font-bold">{{ specialty.title }}</p>
+                        <p class="text-red-600 text-xl md:text-2xl lg:text-3xl font-bold">{{ specialty.title }}</p>
                         <br />
-                        <p class="text-black">{{ specialty.text }}</p>
+                        <p class="text-black text-lg md:text-xl lg:text-2xl">{{ specialty.text }}</p>
                     </div>
                 </li>
             </ul>
