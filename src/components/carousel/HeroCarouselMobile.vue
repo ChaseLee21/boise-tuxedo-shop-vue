fe8d9a33cfacde7ae367beb13f0075b3d241ceb0
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-10 13:01:29 -0700

    implementing lazy loading in the mobile carousel

--- src/components/carousel/HeroCarouselMobile.vue
+++ src/components/carousel/HeroCarouselMobile.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="wrapper flex flex-row overflow-scroll scroll-smooth snap-x snap-mandatory m-0 scroll-m-[1rem]">
         <div v-for="image of images" class="carousel snap-center flex justify-center items-center flex-row flex-nowrap flex-grow-0 flex-shrink-0 flex-auto relative h-auto w-[100vw]" >
-            <img width="390" height="235" class="carousel-image h-[100%] w-auto max-w-[100vw] object-cover object-center" :src="image.url" :alt="image.imageAlt">
+            <v-lazy-image width="390" height="235" class="carousel-image h-[100%] w-auto max-w-[100vw] object-cover object-center" :src="image.url" :alt="image.imageAlt"></v-lazy-image>
         </div>
     </section>
 </template>
@@ -9,6 +9,7 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import $ from 'jquery';
+import VLazyImage from 'v-lazy-image';
 
 let images = ref([]);
 
