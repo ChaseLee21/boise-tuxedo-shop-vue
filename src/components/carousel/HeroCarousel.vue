f729e8f78e8dcdf2be57da965da0d1b23f152028
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-10 13:50:25 -0700

    implemented same solutions in the desk carousel as i did in the mobile carousel

--- src/components/carousel/HeroCarousel.vue
+++ src/components/carousel/HeroCarousel.vue
@@ -9,7 +9,9 @@
         </button>
         <!-- Images -->
         <div v-for="image in images"  class="flex justify-center w-full min-w-fit mx-4">
-            <img class="h-[40rem] max-h-[75vh] min-w-fit object-cover object-center rounded shadow shadow-black" :src="image.url" :alt="image.imageAlt" >
+            <div class="placeholder">
+                <v-lazy-image width="1100" height="640" class="h-[40rem] max-h-[75vh] min-w-fit object-cover object-center rounded shadow shadow-black" :src="image.url" :alt="image.imageAlt"></v-lazy-image>
+            </div>
         </div>
         <!-- Next Button -->
         <button class="sticky top-1/2 right-0" @click="scroll(1)">
@@ -23,9 +25,10 @@
 
 <script setup>
 import { ref, onMounted } from 'vue';
-import $ from 'jquery';
+import VLazyImage from 'v-lazy-image';
 
 let images = ref([]);
+let loading = ref(true);
 
 const imageContainer = ref(null);
 
@@ -38,16 +41,25 @@ const scroll = (direction) => {
 
 // USE: this method to retrieve images from the carousel api route
 onMounted(async () => {
-    await $.ajax({
-        url: 'https://boisetuxedoshop.azurewebsites.net/api/carousel',
-        type: 'GET',
-        error: function (error) {
-            console.log(error);
-        },
-        success: function (data) {
-            images.value = data;
+    try {
+        const response = await fetch('https://boisetuxedoshop.azurewebsites.net/api/carousel');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
-    });
+        const data = await response.json();
+        images.value = data;
+    } catch (error) {
+        console.log(error);
+    } finally {
+        loading.value = false;
+    }
 });
 
 </script>
+<style scoped>
+.placeholder {
+    width: 100%;
+    height: auto;
+    aspect-ratio: 5/3;
+}
+</style>
