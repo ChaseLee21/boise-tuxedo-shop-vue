0e1651882a8366ae15b97b6f4d194668abf13e7b
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-10 13:35:54 -0700

    removing unused imports from my home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,12 +13,8 @@ import HeroCarousel from './carousel/HeroCarousel.vue';
 import ViewProducts from './ViewProducts.vue';
 import GoogleMap from './GoogleMap.vue';
 
-import { ref, onMounted } from 'vue';
-
-import $ from 'jquery';
+import { ref } from 'vue';
 
 // USE: this method to determine if the user is on a mobile device
 let isMobile = ref(window.innerWidth < 768);
-
-
 </script>
