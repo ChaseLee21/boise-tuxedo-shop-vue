49a357681e8d83e189f24c9645a2b1d1a6b3e83b
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-26 11:27:43 -0700

    cleaning up

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,26 +1,19 @@
 <template>
-    <!-- Container  -->
     <section class="my-3 xl:w-[80vw] xl:flex xl:flex-col xl:m-auto">
-        <!-- Category Title -->
         <header v-if="props.title" class="flex flex-row justify-start items-end mx-3">
             <h2 class="text-start text-2xl md:text-3xl lg:text-4xl mx-2 ">{{props.title}}</h2> 
         </header>
-        <!-- Product Container -->
         <article ref="productContainer" class="flex flex-row flex-wrap justify-center items-center mx-2">
-            <!-- Product Images -->
             <div v-for="(image, index) in props.images" :key="index" class="flex-shrink-0 min-w-fit w-full md:w-1/2 lg:w-1/3 xl:w-1/4 m-2 p-2 rounded-md bg-slate-800">
-                <img :src="image.url" :alt="image.alt" class="w-full h-auto rounded-md" />
+                <v-lazy-image :src="image.url" :alt="image.alt" class="w-full h-auto rounded-md" />
             </div>
         </article>
-
     </section>
 </template>
 
-<style scoped>
-</style>
-
 <script setup>
 import { ref } from 'vue';
+import VLazyImage from 'v-lazy-image';
 
 const props = defineProps({
     title: {
@@ -35,4 +28,6 @@ const props = defineProps({
 
 const productContainer = ref(null);
 
+//TODO: update styling of images and containers 
+//TODO: remove unneccessary styling
 </script>
