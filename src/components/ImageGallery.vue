f65ca5ce01c0dfbf88a5b7b604636177287cdd5a
Tiny Penguin <chase.seeberger@gmail.com>
2024-04-26 10:48:13 -0700

    update: imageGallery component takes in a prop of images to display

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -8,7 +8,7 @@
         <!-- Product Container -->
         <article ref="productContainer" class="flex flex-row flex-wrap justify-center items-center mx-2">
             <!-- Product Images -->
-            <div v-for="(product, index) in products" :key="index" class="flex-shrink-0 min-w-fit w-full md:w-1/2 lg:w-1/3 xl:w-1/4 m-2 p-2 rounded-md bg-slate-800">
+            <div v-for="(product, index) in props.products" :key="index" class="flex-shrink-0 min-w-fit w-full md:w-1/2 lg:w-1/3 xl:w-1/4 m-2 p-2 rounded-md bg-slate-800">
                 <img :src="product.url" :alt="product.alt" class="w-full h-auto rounded-md" />
             </div>
         </article>
@@ -27,34 +27,11 @@ const props = defineProps({
         type: String,
         required: true
     },
-})
-
-const products = [
-    {
-        url: "https://btsimagestorage.blob.core.windows.net/product-images/Prom-Couple-Orange-Accessories-Navy-Suit-Tangerine-Dress.webp",
-        alt: "Prom Couple Orange Accessories Navy Suit Tangerine Dress"
-    },
-    {
-        url: "https://btsimagestorage.blob.core.windows.net/product-images/Prom-Popular-Outfit-Choices-Tuxedo-Suits-Boise-Meridian-High-School (2).webp",
-        alt: "Young man in a silver black imperial paisley one button shawl tuxedo"
-    },
-    {
-        url: "https://btsimagestorage.blob.core.windows.net/product-images/Prom-Popular-Outfit-Choices-Tuxedo-Suits-Boise-Meridian-High-School (3).webp",
-        alt: "Prom goers wearing tuxedos with bow ties at the Boise Train Depot"
-    },
-    {
-        url: "https://btsimagestorage.blob.core.windows.net/product-images/Prom-Popular-Outfit-Choices-Tuxedo-Suits-Boise-Meridian-High-School (4).webp",
-        alt: "Three prom goers in tuxedos and dresses in front of the entrance to their prom venue"
-    },
-    {
-        url: "https://btsimagestorage.blob.core.windows.net/product-images/Prom-Popular-Outfit-Choices-Tuxedo-Suits-Boise-Meridian-High-School (5).webp",
-        alt: "Teenagers in tuxedos with bow ties and sunglasses posing for a photo at prom"
-    },
-    {
-        url: "https://btsimagestorage.blob.core.windows.net/product-images/Prom-Popular-Outfit-Choices-Tuxedo-Suits-Boise-Meridian-High-School (6).webp",
-        alt: "Young man in a navy suit and tie with a white shirt and black shoes posing by his car"
+    products: {
+        type: Array,
+        required: true
     }
-]
+})
 
 const productContainer = ref(null);
 
