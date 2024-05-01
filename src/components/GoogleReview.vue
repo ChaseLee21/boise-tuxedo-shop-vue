baf086dcc2cf6a38ef23af1b575edb7550e6e98b
Tiny Penguin <chase.seeberger@gmail.com>
2024-05-01 14:55:30 -0700

    updating styling of google reviews component

--- src/components/GoogleReview.vue
+++ src/components/GoogleReview.vue
@@ -1,16 +1,11 @@
 <template>
-    <section class="bg-gray-800 text-white p-10   text-center">
-            <p class="text-3xl md:text-4xl lg:text-5xl font-bold">The Boise Tuxedo Shop Experience:</p>
-            <br class="line-through" />
-            <ul class="flex flex-row overflow-x-auto md:grid md:grid-flow-row md:grid-cols-2 lg:grid-cols-4 ">
-                <li class="bg-white border rounded m-2 p-4 md:row-span-1 md:hover:drop-shadow-xl" v-for="review in reviews" :key="review.id">
-                    <div class="w-52 md:w-auto ">
-                        <p class="text-red-600 text-xl md:text-2xl lg:text-3xl font-bold">{{ review.rating }} Stars, {{ review.relativePublishTimeDescription }}:</p>
-                        <br />
-                        <p class="text-black text-lg md:text-xl lg:text-2xl">{{ review.text.text }}</p>
-                    </div>
-                </li>
-            </ul>
+    <section class="">
+        <ul class="xl:w-[80vw] flex flex-col  m-auto">
+            <li class="m-2 p-2 border-black border shadow-lg rounded" v-for="review in reviews" :key="review.id">
+                <p class="text-red-600 text-xl md:text-2xl lg:text-3xl font-bold">{{ review.rating }} Stars, {{ review.relativePublishTimeDescription }}:</p>
+                <p class="text-lg md:text-xl lg:text-2xl">{{ review.text.text }}</p>
+            </li>
+        </ul>
     </section>
 </template>
 
@@ -21,7 +16,6 @@ const reviews = ref([]);
 
 onMounted(async () => {
     reviews.value = await setGoogleReviews();
-    console.log(reviews.value);
 });
 
 async function setGoogleReviews() {
