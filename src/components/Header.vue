4bb7fd5abaf8edad1ab297969e3befa41acea141
Tiny Penguin <chase.seeberger@gmail.com>
2024-05-08 16:36:18 -0700

    removed the social media icons from the header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,16 +3,6 @@
         <section class="md:col-span-1 flex flex-row justify-center">
             <img height="200px" width="168px" src="/Tuxedo-Shop-Logo-3.webp" alt="Logo including a top hat, part of a suit, and the words Boise Tuxedo Shop." >
         </section>
-        <section v-if="!isMobile" class="md:col-span-1 m-auto">
-            <div class="flex justify-center">
-                <a class="p-2" href="https://www.facebook.com/boisetuxedoshop/" target="_blank" rel="noopener noreferrer">
-                    <img width="32px" height="32px" src="/Facebook_Logo_Primary.webp" alt="Facebook" />
-                </a>
-                <a class="p-2" href="https://www.instagram.com/boisetuxedoshop/" target="_blank" rel="noopener noreferrer">
-                    <img width="32px" height="32px" src="/Instagram_Glyph_Gradient.webp" alt="Instagram" />
-                </a>
-            </div>
-        </section>
     </header>
 </template>
 
