eb0f27c624fd51f2c7e73f74b5bd909d4fd8dfa7
Tiny Penguin <chase.seeberger@gmail.com>
2024-05-08 14:25:32 -0700

    changed logo size in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,15 +1,15 @@
 <template>
     <header class="flex flex-col md:grid md:grid-cols-3">
-        <section class="col-span-1 m-auto">
+        <!-- <section class="col-span-1 m-auto">
             <ul class="flex flex-col text-center justify-center m-auto text-xl font-bold">
                 <li class="text-red-600">Over 4000 Tuxedo and Suit Styles in Stock</li>
                 <li>5 Star Customer Service</li>
                 <li>No Appointment Necessary</li>
                 <li>Guaranteed Best Prices In The Treasure Valley</li>
             </ul>
-        </section>
+        </section> -->
         <section class="md:col-span-1 flex flex-row justify-center">
-            <img height="300px" width="252px" src="/Tuxedo-Shop-Logo-3.webp" alt="Logo including a top hat, part of a suit, and the words Boise Tuxedo Shop." >
+            <img height="200px" width="168px" src="/Tuxedo-Shop-Logo-3.webp" alt="Logo including a top hat, part of a suit, and the words Boise Tuxedo Shop." >
         </section>
         <section v-if="!isMobile" class="md:col-span-1 m-auto">
             <div class="flex justify-center">
