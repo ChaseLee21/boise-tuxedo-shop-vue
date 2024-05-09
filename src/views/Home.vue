0bf2b57590eccf700ce5b145bccceb22597f4866
Tiny Penguin <chase.seeberger@gmail.com>
2024-05-09 16:45:04 -0700

    implemented header component in the home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,4 +1,5 @@
 <template>
+    <Header title = 'Tuxedo and Suits for Rent or Purchase' content = "The Treasure Valley's First Choice for Formal Attire" />
     <main>
         <Experience />
         <HeroCarousel v-if="!isMobile"/>
@@ -12,7 +13,7 @@ import HeroCarouselMobile from '../components/carousel/HeroCarouselMobile.vue';
 import HeroCarousel from '../components/carousel/HeroCarousel.vue';
 import Experience from '../components/Experience.vue';
 import GoogleMap from '../components/GoogleMap.vue';
-
+import Header from '../components/Header.vue';
 import { ref } from 'vue';
 
 // USE: this method to determine if the user is on a mobile device
