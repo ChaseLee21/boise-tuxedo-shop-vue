e55b307d138cf9c2d336df87219e6e93f4c3bc2c
Tiny Penguin <chase.seeberger@gmail.com>
2024-05-09 15:37:53 -0700

    updating contact view to use new header component

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,16 +1,24 @@
 <template>
+    <Header :title="headerProps.title" :content="headerProps.content" :links="headerProps.links" />
     <main class="xl:w-[80vw] xl:flex xl:flex-col xl:m-auto">
-        <header class="mb-2">
-            <h1 class="text-2xl md:text-3xl lg:text-4xl font-bold underline py-2">Contact Us</h1>
-            <p class="md:text-lg lg:text-xl px-2">
-                Please refer to the <router-link class="text-blue-900 font-bold underline" to="/Faq">FAQ's</router-link> before calling to see if your question is already answered!
-                If you are a part of a wedding party and are ready to give us your measurements, please send us an email with your name, wedding date, groom's name, and your measurements.
-                If you still have questions or concerns, please call us or send us an email and we will get back to you as soon as possible!
-            </p>
-        </header>
         <section role="contentinfo" class="flex flex-col">
-            <a class="text-blue-900 font-bold underline px-2" href="tel:208-375-9616">Call: (208) 375-9616</a>
-            <a class="text-blue-900 font-bold underline px-2" href="mailto:boisetuxedoshop@gmail.com" target="_blank">Email: boisetuxedoshop@gmail.com</a>
+            <ul class="text-lg list-inside list-disc mx-2 indent-2">
+                <li><a class="link" href="tel:208-375-9616">Call: (208) 375-9616</a></li>
+                <li><a class="link" href="mailto:boisetuxedoshop@gmail.com" target="_blank">Email: boisetuxedoshop@gmail.com</a></li>
+            </ul>
         </section>
     </main>
 </template>
+<script setup>
+import Header from '../components/Header.vue';
+
+const headerProps = {
+    title: 'Contact Us',
+    content: `Please refer to the frequently asked questions section before calling to see if your question is already answered!
+    If you are a part of a wedding party and are ready to give us your measurements, please send us an email with your name, wedding date, groom's name, and your measurements.
+    If you still have questions or concerns, please call us or send us an email and we will get back to you as soon as possible!`,
+    links: [
+        { url: '/Faq', text: 'FAQ\'s' }
+    ]
+}
+</script>
