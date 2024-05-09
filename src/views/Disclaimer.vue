287eb84f34cdc1b88dfe9d503003b36f694e4aec
Tiny Penguin <chase.seeberger@gmail.com>
2024-05-09 15:46:30 -0700

    updated disclaimer, privacy, and terms views with header component

--- src/views/Disclaimer.vue
+++ src/views/Disclaimer.vue
@@ -1,5 +1,15 @@
 <template>
-  <a class="flex justify-center underline text-blue-600" href="https://app.termly.io/document/disclaimer/a782a7fa-f1ad-4b68-bc9c-1beb07bcd2ad">
-    Click here to read our disclaimer.
-  </a>
+  <Header :title="headerProps.title" />
+  <main class="xl:w-[80vw] xl:flex xl:flex-col xl:m-auto mx-2">
+    <a class="link" href="https://app.termly.io/document/disclaimer/a782a7fa-f1ad-4b68-bc9c-1beb07bcd2ad">
+      Click here to read our disclaimer.
+    </a>
+  </main>
 </template>
+<script setup>
+import Header from '../components/Header.vue';
+
+const headerProps = {
+  title: `Disclaimer`,
+}
+</script>
