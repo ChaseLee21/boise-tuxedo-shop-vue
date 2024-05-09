8c40c383c308481f9f9330457580ed856491970d
Tiny Penguin <chase.seeberger@gmail.com>
2024-05-09 12:38:52 -0700

    using v-html instead

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,7 @@
 <template>
     <header class="my-3 xl:w-[80vw] xl:flex xl:flex-col xl:m-auto">
         <h1 class="my-2 text-start text-2xl md:text-3xl lg:text-4xl mx-2 font-bold underline" >{{ props.title }}</h1>
-        <p class="text-start md:text-lg lg:text-xl ms-5 my-2">
-            {{ props.content }}
-        </p>
+        <p v-html="props.content" class="text-start md:text-lg lg:text-xl ms-5 my-2"></p>
     </header>
 </template>
 
@@ -14,7 +12,7 @@
             required: true
         },
         content: {
-            type: String,
+            type: HTMLAllCollection,
             required: false
         }
     });
