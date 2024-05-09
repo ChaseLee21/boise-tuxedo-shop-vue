9f47457b973003a8ee813156b3a639914577a896
Tiny Penguin <chase.seeberger@gmail.com>
2024-05-09 16:35:22 -0700

    updating category page product cards to match home pacge experience component cards

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -2,35 +2,16 @@
     <Header v-if="props.category === 'TuxedoSuit'" title="Tuxedos & Suits" />
     <Header v-else :title="props.category" />
     <main class="m-3 xl:w-[80vw] xl:flex xl:flex-col xl:m-auto">
-        <section class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-            <!-- Product Card -->
-            <article class="flex flex-col justify-evenly bg-gray-800 text-white text-center font-bold m-4 px-3 shadow-lg shadow-black rounded items-center" v-for="product in filterProducts">
-                <router-link class="w-full flex flex-col justify-center items-center" 
-                :to="{ name: 'Product', params: {id: product.id } }">
-                    <p class="p-1 text-lg md:text-xl lg:text-2xl">{{ product.name }}</p>
-                    <img class="h-[40rem] max-h-[75vh] w-fit object-cover object-center rounded shadow shadow-black" :src="product.imageURL" :alt="product.imageAlt" >
-                </router-link>
 
-                <!-- Pricing -->
-                <div v-if="product.retailPrice && !product.saleRetailPrice && !product.rentalPrice" class="">
-                    <p>Purchase Price: ${{ product.retailPrice }}</p>
-                </div>
-                <div v-if="product.retailPrice && product.saleRetailPrice && !product.rentalPrice" class="flex justify-between w-full px-2">
-                    <p>Purchase Price: <span class="line-through">${{ product.retailPrice }}</span></p>
-                    <p class="text-red-600">On Sale: ${{ product.saleRetailPrice }}</p>
-                </div>
-                <div v-if="product.retailPrice && !product.saleRetailPrice && product.rentalPrice" class="flex justify-between w-full px-2">
-                    <p>Purchase Price: ${{ product.retailPrice }}</p>
-                    <p>Rental Price: ${{ product.rentalPrice }}</p>
-                </div>
-                <div v-if="product.retailPrice && product.saleRetailPrice && product.rentalPrice" class="flex justify-between w-full px-2 text-center">
-                    <p>Purchase Price: <span class="line-through">${{ product.retailPrice }}</span></p>
-                    <p class="text-red-600">On Sale: ${{ product.saleRetailPrice }}</p>
-                    <p >Rental Price: ${{ product.rentalPrice }}</p>
-                </div>
-                <div v-if="product.rentalPrice && !product.retailPrice && !product.saleRetailPrice">
-                    <p>Rental Price: ${{ product.rentalPrice }}</p>
-                </div>
+        <section class="sm:flex sm:flex-col md:h-full md:grid md:grid-flow-row md:grid-cols-2 md:w-[90%] lg:w-[80%] xl:grid-cols-4 md:mx-auto mb-8">
+            <article v-for="product in filterProducts" class="min-w-fit my-5 mx-1 rounded-md snap-center relative">
+                <figure class=" text-white text-center h-full">
+                    <!-- Image -->
+                    <v-lazy-image width="720" height="1200" :src="product.imageURL" :alt="product.imageAlt" class="m-auto min-w-full min-h-full rounded object-cover" />
+                    <!-- Title -->
+                    <figcaption class="absolute top-0 inset-x-0 w-full p-0.5 text-lg rounded-t bg-opacity-30 m-auto bg-black">{{ product.name }}</figcaption>
+                    <router-link :to="{ name: 'Product', params: {id: product.id } }" class="absolute bottom-0 inset-x-0 w-full p-0.5 rounded-b bg-opacity-30 m-auto bg-black">View Style Details</router-link>
+                </figure>
             </article>
         </section>
     </main>
@@ -39,6 +20,7 @@
 <script setup>
 import { onMounted, computed, ref } from 'vue';
 import Header from '../components/Header.vue';
+import vLazyImage from 'v-lazy-image';
 
 const props = defineProps({
     category: {
@@ -68,4 +50,37 @@ async function getProducts() {
     }
     products.value = data;
 }
+</script>
+<script>
+        // <section class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+        //     <!-- Product Card -->
+        //     <article class="flex flex-col justify-evenly bg-gray-800 text-white text-center font-bold m-4 px-3 shadow-lg shadow-black rounded items-center" v-for="product in filterProducts">
+        //         <router-link class="w-full flex flex-col justify-center items-center" 
+        //         :to="{ name: 'Product', params: {id: product.id } }">
+        //             <p class="p-1 text-lg md:text-xl lg:text-2xl">{{ product.name }}</p>
+        //             <img class="h-[40rem] max-h-[75vh] w-fit object-cover object-center rounded shadow shadow-black" :src="product.imageURL" :alt="product.imageAlt" >
+        //         </router-link>
+
+        //         <!-- Pricing -->
+        //         <div v-if="product.retailPrice && !product.saleRetailPrice && !product.rentalPrice" class="">
+        //             <p>Purchase Price: ${{ product.retailPrice }}</p>
+        //         </div>
+        //         <div v-if="product.retailPrice && product.saleRetailPrice && !product.rentalPrice" class="flex justify-between w-full px-2">
+        //             <p>Purchase Price: <span class="line-through">${{ product.retailPrice }}</span></p>
+        //             <p class="text-red-600">On Sale: ${{ product.saleRetailPrice }}</p>
+        //         </div>
+        //         <div v-if="product.retailPrice && !product.saleRetailPrice && product.rentalPrice" class="flex justify-between w-full px-2">
+        //             <p>Purchase Price: ${{ product.retailPrice }}</p>
+        //             <p>Rental Price: ${{ product.rentalPrice }}</p>
+        //         </div>
+        //         <div v-if="product.retailPrice && product.saleRetailPrice && product.rentalPrice" class="flex justify-between w-full px-2 text-center">
+        //             <p>Purchase Price: <span class="line-through">${{ product.retailPrice }}</span></p>
+        //             <p class="text-red-600">On Sale: ${{ product.saleRetailPrice }}</p>
+        //             <p >Rental Price: ${{ product.rentalPrice }}</p>
+        //         </div>
+        //         <div v-if="product.rentalPrice && !product.retailPrice && !product.saleRetailPrice">
+        //             <p>Rental Price: ${{ product.rentalPrice }}</p>
+        //         </div>
+        //     </article>
+        // </section>
 </script>
