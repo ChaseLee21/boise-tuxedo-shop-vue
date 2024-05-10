c3970ba9a4b76b0ee2043aba45e1991f732045f3
Tiny Penguin <chase.seeberger@gmail.com>
2024-05-10 12:29:01 -0700

    created svg of up / down arrow in desktop navbar to easily show user rent or buy is a dropdown menu

--- src/components/Navigation Bar/NavBar.vue
+++ src/components/Navigation Bar/NavBar.vue
@@ -4,7 +4,23 @@
             <ul class="flex flex-row flex-wrap justify-center items-center text-lg p-3">
                 <li class="lg:text-2xl text-xl font-bold hover:text-zinc-300 mx-2"><router-link to="/" >Boise Tuxedo Shop</router-link></li>
                 <div class="relative">
-                    <li id="rentOrBuyLink" class="hover:text-zinc-300 mx-2" ><router-link to="/RentOrBuy">Rent or Buy</router-link></li>
+                    <li id="rentOrBuyLink" class="hover:text-zinc-300 mx-2" >
+                        <router-link to="/RentOrBuy">
+                            <div class="flex items-center relative me-2">
+                                Rent or Buy
+                                <Transition>
+                                    <svg v-if="!rentOrBuyNavOpen" width="10" height="10" class="absolute -right-3 stroke-white stroke-[2px] scale-75">
+                                        <line x1="0" y1="0" x2="5" y2="10"></line>
+                                        <line x1="10" y1="0" x2="5" y2="10"></line>
+                                    </svg>
+                                    <svg v-else width="10" height="10" class="absolute -right-3 stroke-white stroke-[2px] scale-75">
+                                        <line x1="0" y1="10" x2="5" y2="0"></line>
+                                        <line x1="10" y1="10" x2="5" y2="0"></line>
+                                    </svg>
+                                </Transition>             
+                            </div>
+                        </router-link>
+                    </li>
                     <Transition>
                         <ul id="rentOrBuyNavList" v-show="rentOrBuyNavOpen" class="absolute bg-slate-800 p-2 rounded w-48">
                             <li class="hover:text-zinc-300 w-full"><router-link class="block w-full" to="/Category/TuxedoSuit">Tuxedos & Suits</router-link></li>
