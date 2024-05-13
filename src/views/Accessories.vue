ff7136bab1d45a93bc7720f4447ea2600c0eadc6
Tiny Penguin <chase.seeberger@gmail.com>
2024-05-13 10:15:03 -0700

    adding the accessories view as a route

---
+++ src/views/Accessories.vue
@@ -0,0 +1,11 @@
+<template>
+    <Header :title = "headerProps.title" />
+</template>
+<script setup>
+import Header from '../components/Header.vue'
+
+const headerProps = {
+  title: 'Accessories'
+}
+
+</script>
