2ed27e12ada8d28842817e16cf3a45001eadba32
Tiny Penguin <chase.seeberger@gmail.com>
2024-05-13 16:11:36 -0700

    creating component that takes in an array of colorSwatches

---
+++ src/components/colorSwatches.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+        <ul>
+            <li v-for="swatch in props.colorSwatches" class="w-fit flex items-center hover:underline cursor-pointer" :key="swatch.color">
+                <svg class="w-12 h-12  ">
+                <circle cx="25" cy="25" r="16" />
+                <circle cx="25" cy="25" r="15"  :fill="swatch.hexCode" />
+                </svg>
+                <span>{{ swatch.color }}</span>
+            </li>
+        </ul>
+    </section>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    colorSwatches: {
+        type: Array,
+        required: true
+    }
+})
+
+</script>
