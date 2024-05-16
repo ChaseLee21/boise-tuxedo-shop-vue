08c5780f38863da69d469b63a0d6d2d2db3d089e
Tiny Penguin <chase.seeberger@gmail.com>
2024-05-16 10:10:01 -0700

    update: made the imagePopUp component take in props and emit a closed event

--- src/components/ImagePopUp.vue
+++ src/components/ImagePopUp.vue
@@ -1,17 +1,40 @@
 <template>
     <section v-if="popUpOpen" class="fixed inset-0 bg-slate-800 bg-opacity-30 rounded m-auto h-screen w-fit px-8 pb-10 flex justify-center items-center">
         <figure class="max-h-[600px] w-auto flex flex-col items-center shadow-xl rounded border border-black relative" >
-            <svg v-on:click="popUpOpen = !popUpOpen" xmlns="http://www.w3.org/2000/svg" width="50" height="50" class="stroke-black stroke-2 absolute right-0 top-0 cursor-pointer hover:stroke-[3px]">
+            <svg v-on:click="handlePopUpClose()" xmlns="http://www.w3.org/2000/svg" width="50" height="50" class="stroke-black stroke-2 absolute right-0 top-0 cursor-pointer hover:stroke-[3px]">
                     <line x1="13" y1="15" x2="32" y2="30"></line>
                     <line x1="13" y1="30" x2="32" y2="15"></line> 
             </svg>
-            <figcaption class="text-xl lg:text-2xl font-bold py-2 bg-white w-full text-center">Burgundy</figcaption>
-            <img width="600" height="600" :src="testImageUrl" alt="Burgundy Pocket Square" class="" />
+            <figcaption class="text-xl lg:text-2xl font-bold py-2 bg-white w-full text-center">{{ props.title }}</figcaption>
+            <img width="600" height="600" :src="props.url" :alt="props.alt" class="" />
         </figure>
     </section>
 </template>
 <script setup>
-import {ref} from 'vue';
+import {ref, defineEmits} from 'vue';
+
+const props = defineProps({
+    url: {
+        type: String,
+        required: true
+    },
+    alt: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    }
+})
+
+const emit = defineEmits(['PopUpClosed']);
+
 let popUpOpen = ref(true);
+
+function handlePopUpClose() {
+    popUpOpen.value = !popUpOpen.value;
+    emit('PopUpClosed');
+}
 
 </script>
