c3b9ed17905ed6949c7778bea98edb2cb5aac924
Tiny Penguin <chase.seeberger@gmail.com>
2024-05-16 10:10:14 -0700

    implementing new imagePopUp component

--- src/components/ColorSwatches.vue
+++ src/components/ColorSwatches.vue
@@ -1,7 +1,7 @@
 <template>
     <article>
         <ul>
-            <li v-for="swatch in props.colorSwatches" class="w-fit flex items-center hover:cursor-pointer hover:underline" :key="swatch.color">
+            <li v-for="swatch in props.colorSwatches" class="w-fit flex items-center hover:cursor-pointer hover:underline" :key="swatch.color" v-on:click="openPopUp()">
                 <svg class="w-12 h-12">
                     <circle cx="25" cy="25" r="16" />
                     <circle cx="25" cy="25" r="15"  :fill="swatch.hexCode" />
@@ -10,14 +10,25 @@
             </li>
         </ul>
     </article>
+    <ImagePopUp v-if="popUpOpen" :url="testImageUrl" alt="Test Image" title="Test Image" @PopUpClosed="popUpOpen = false" />
 </template>
 <script setup>
+import ImagePopUp from './ImagePopUp.vue';
+import {ref} from 'vue';
+
+let popUpOpen = ref(false);
+
 const props = defineProps({
     colorSwatches: {
         type: Array,
         required: true
     }
 })
+
+function openPopUp() {
+    console.log('openPopUp', popUpOpen);
+    popUpOpen.value = !popUpOpen.value;
+}
 
 const testImageUrl = 'https://btsimagestorage.blob.core.windows.net/product-images/Boise-Tuxedo-Shop-Burgundy-Color-Pocket-Square.webp';
 
