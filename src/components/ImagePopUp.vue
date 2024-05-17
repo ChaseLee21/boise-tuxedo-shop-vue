c48867a336368009378c6a53f060c63920239e09
Tiny Penguin <chase.seeberger@gmail.com>
2024-05-17 10:00:06 -0700

    fix: removed border that had a visual glitch to it fix: x svg that was streched on the x axis update: rounded the edges of the image to match the design of the rest of the website

--- src/components/ImagePopUp.vue
+++ src/components/ImagePopUp.vue
@@ -1,12 +1,12 @@
 <template>
     <section v-if="popUpOpen" class="fixed inset-0 bg-slate-800 bg-opacity-30 rounded m-auto h-screen w-screen px-8 pb-10 flex justify-center items-center">
-        <figure class="max-h-[600px] w-auto flex flex-col items-center shadow-xl rounded border border-black relative" >
-            <svg v-on:click="handlePopUpClose()" xmlns="http://www.w3.org/2000/svg" width="50" height="50" class="stroke-black stroke-2 absolute right-0 top-0 cursor-pointer hover:stroke-[3px]">
-                    <line x1="13" y1="15" x2="32" y2="30"></line>
-                    <line x1="13" y1="30" x2="32" y2="15"></line> 
+        <figure class="max-h-[600px] w-auto flex flex-col items-center shadow-xl relative" >
+            <svg v-on:click="handlePopUpClose()" xmlns="http://www.w3.org/2000/svg" width="15" height="15" class="stroke-slate-800 stroke-2 m-4 absolute right-0 top-0 cursor-pointer hover:stroke-[3px]">
+                    <line x1="0" y1="0" x2="15" y2="15"></line>
+                    <line x1="0" y1="15" x2="15" y2="0"></line> 
             </svg>
-            <figcaption class="text-xl lg:text-2xl font-bold py-2 bg-white w-full text-center">{{ props.title }}</figcaption>
-            <img width="600" height="600" :src="props.url" :alt="props.alt" class="" />
+            <figcaption class="text-xl lg:text-2xl font-bold py-2 bg-white w-full text-center rounded-t">{{ props.title }}</figcaption>
+            <img width="600" height="600" :src="props.url" :alt="props.alt" class="rounded-b" />
         </figure>
     </section>
 </template>
