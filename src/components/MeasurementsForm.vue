1c3645acdd3cc4286b87da883664fbfb2fe3a930
Chase Seeberger <chase.seeberger@gmail.com>
2025-01-21 15:59:13 -0800

    updating variable names to be consistent

--- src/components/MeasurementsForm.vue
+++ src/components/MeasurementsForm.vue
@@ -2,15 +2,40 @@
     <main class="xl:w-[80vw] xl:flex xl:flex-col xl:m-auto mx-2 bg-gray-800 rounded p-1">
         <form class="m-2">
             <div class="flex-col my-1">
-                <label class="text-white mx-1">Name</label>
-                <input id="nameInput" class="w-full rounded p-1" type="text" placeholder="John Doe">
+                <label class="text-white mx-1 text-lg">First and Last Name</label>
+                <input id="name" class="w-full rounded p-1" type="text" placeholder="John Doe">
             </div>
             <div class="flex-col my-1">
-                <label class="text-white mx-1">Height: </label>
-                <input class="w-auto bg-gray-800 text-white" id="heightTextInput" type="text" :value="heightString" readonly>
+                <label class="text-white mx-1 text-lg">Email</label>
+                <label class="text-white opacity-80 text-sm">we will only email you if we have any questions about your measurements</label>
+                <input id="email" class="w-full rounded p-1" type="text" placeholder="JohnDoe@gmail.com">
+            </div>
+            <div class="flex-col my-1">
+                <label class="text-white mx-1 text-lg">Wedding / Event Name</label>
+                <label class="text-white opacity-80 text-sm">generally, this is the name of the bride and groom</label>
+                <input id="eventName" class="w-full rounded p-1" type="text" placeholder="John & Jane Doe's Wedding">
+            </div>
+            <div class="flex-col my-1">
+                <label class="text-white mx-1 text-lg">Wedding / Event Role</label>
+                <label class="text-white opacity-80 text-sm"></label>
+                <input id="eventRole" class="w-full rounded p-1" type="text" placeholder="Ex. Groomsman, Father of the Bride">
+            </div>
+            <div class="flex-col my-1">
+                <label class="text-white mx-1 text-lg">Height: </label>
+                <input class="w-auto bg-gray-800 text-white" id="heightString" type="text" :value="heightString" readonly>
                 <div>
-                    <input id="heightRangeInput" type="range" min="1" max="72" v-model="heightValue" />
+                    <input id="heightRange" type="range" min="1" max="72" v-model="heightValue" />
                 </div>
+            </div>
+            <div class="flex-col my-1">
+                <label class="text-white mx-1 text-lg">Weight</label>
+                <label class="text-white opacity-80 text-sm">Please enter weight in pounds (lbs)</label>
+                <input id="weight" class="w-full rounded p-1" type="number" min="10" max="400" placeholder="200">
+            </div>
+            <div class="flex-col my-1">
+                <label class="text-white mx-1 text-lg">Shoe Size</label>
+                <label class="text-white opacity-80 text-sm"></label>
+                <input id="shoeSize" class="w-full rounded p-1" type="number" min="10" max="400" placeholder="200">
             </div>
         </form>
     </main>
@@ -20,7 +45,7 @@
 import { ref, onMounted, computed } from 'vue';
 import { convertRangeInputToHeight } from '../utils/helpers'
 
-const heightValue = ref(36); // Default value
+const heightValue = ref(46); // Default value
 const heightString = computed(() => {
     return convertRangeInputToHeight(heightValue.value)
 })
--- src/components/Tests/MeasurementsForm.test.js
+++ src/components/Tests/MeasurementsForm.test.js
@@ -5,22 +5,22 @@ import MeasurementsForm from '../MeasurementsForm.vue';
 describe('MeasurementsForm.vue', () => {
   it('renders the initial height value correctly from the range input', () => {
     const wrapper = mount(MeasurementsForm);
-    const heightRangeInput = wrapper.find('input[id="heightRangeInput"]');
-    expect(heightRangeInput.element.value).toBe('37');
+    const heightRange = wrapper.find('input[id="heightRange"]');
+    expect(heightRange.element.value).toBe('46');
   });
 
   it('renders the initial height value correctly from the text input', () => {
     const wrapper = mount(MeasurementsForm);
-    const heightTextInput = wrapper.find('input[id="heightTextInput"]');
-    expect(heightTextInput.element.value).toBe(`5'0"`);
+    const heightString = wrapper.find('input[id="heightString"]');
+    expect(heightString.element.value).toBe(`5'9"`);
   });
 
   it('updates the height value when the range input is changed', async () => {
     const wrapper = mount(MeasurementsForm);
-    const heightRangeInput = wrapper.find('input[id="heightRangeInput"]');
-    const heightTextInput = wrapper.find('input[id="heightTextInput"]');
-    await heightRangeInput.setValue(50);
-    expect(heightTextInput.element.value).toBe(`6'1"`);
-    expect(heightRangeInput.element.value).toBe('50');
+    const heightRange = wrapper.find('input[id="heightRange"]');
+    const heightString = wrapper.find('input[id="heightString"]');
+    await heightRange.setValue(50);
+    expect(heightString.element.value).toBe(`6'1"`);
+    expect(heightRange.element.value).toBe('50');
   });
 });
