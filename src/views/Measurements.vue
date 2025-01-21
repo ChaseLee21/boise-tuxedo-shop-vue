4ae5e603c7fee1dd9c960056ae98a41717bc35d6
Chase Seeberger <chase.seeberger@gmail.com>
2025-01-21 13:49:18 -0800

    Added new Measurements component

---
+++ src/views/Measurements.vue
@@ -0,0 +1,27 @@
+<template>
+    <Header :title = "headerProps.title" :content = "headerProps.content" :links = "headerProps.links" />
+    <main class="xl:w-[80vw] xl:flex xl:flex-col xl:m-auto mx-2">
+        <section role="contentinfo">
+            <article>
+                <h2>Coming Soon</h2>
+            </article>
+        </section>
+    </main>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+import Header from '../components/Header.vue';
+
+const headerProps = {
+    title: `Submit Your Measurements`,
+    content: `If you are attending a wedding or event that is booked with us filling out the form below is the best way to ensure your measurements reach our hands! You should receieve a confirmation email shortly after submitting the form. If you did not recieve this confirmation email please contact us.`,
+    links: [
+        { url: '/Contact', text: 'Contact Us'}
+    ]
+}
+
+onMounted(() => {
+    
+})
+</script>
