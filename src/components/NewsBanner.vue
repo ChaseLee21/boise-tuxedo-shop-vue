49f6bdaf616c51277fea6ad2ae32e76a8f904df7
Chase Seeberger <chase.seeberger@gmail.com>
2025-02-10 11:54:19 -0800

    Updating style and text of news banner component

--- src/components/NewsBanner.vue
+++ src/components/NewsBanner.vue
@@ -1,5 +1,7 @@
 <template>
-    <p>News Banner</p>
+    <div class="w-full text-center bg-red-600 bg-opacity-75">
+        <p class="font-bold" v-if="bannerText != ''">{{ bannerText }}</p>
+    </div>
 </template>
 
 <script setup>
@@ -7,5 +9,6 @@ import { ref } from 'vue';
 
 let isMobile = ref(window.innerWidth < 768);
 
+let bannerText = ref("Booking rentals for Weddings and Proms now! Come in today to get complimentary measurements and choose your style from our collection of over 4000 suits and tuxedos!")
 
 </script>
