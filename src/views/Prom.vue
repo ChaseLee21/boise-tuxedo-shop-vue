8b0b9c9a682797c1ada735dc133c5480cb910c42
Chase Seeberger <chase.seeberger@gmail.com>
2025-02-19 12:35:10 -0800

    Updating prom information to be accurate to 2025 prom season

--- src/views/Prom.vue
+++ src/views/Prom.vue
@@ -6,23 +6,23 @@
             <div class="px-2">
                 <article>
                     <h3 class="text-2xl text-red-600 font-bold">How soon do I need to reverse my tuxedo or suit?</h3>
-                    <p class="indent-3">Ideally 2 to 3 weeks before prom, but we own all of our outfits and can always fit you even the week of prom.</p>
+                    <p class="indent-3 text-lg">Ideally 2 to 3 weeks before prom, but we own all of our outfits and can always fit you even the week of prom.</p>
                 </article>
                 <article>
                     <h3 class="text-2xl text-red-600 font-bold">Do you only rent?</h3>
-                    <p class="indent-3">We do both rental and purchase, we carry over 3,000 tuxedos and suits in stock for rental and over 700 suits for purchase.</p>
+                    <p class="indent-3 text-lg">We do both rental and purchase, we carry over 3,000 tuxedos and suits in stock for rental and over 700 suits for purchase.</p>
                 </article>
                 <article>
                     <h3 class="text-2xl text-red-600 font-bold">How much does it cost to rent a tuxedo or suit for prom?</h3>
-                    <p class="indent-3">Please refer to our price chart below for a detailed overview of our prom 2024 pricing</p>
+                    <p class="indent-3 text-lg">Please refer to our price chart below for a detailed overview of our prom 2024 pricing</p>
                 </article>
                 <article>
                     <h3 class="text-2xl text-red-600 font-bold">When do I need to pay for my rental?</h3>
-                    <p class="indent-3">We only require a 20.00 Deposit at the time you reserve and the balance is paid when you do your final fitting. If you rather pay for it when you reserve it that is okay as well.</p>
+                    <p class="indent-3 text-lg">We only require a 20.00 Deposit at the time you reserve and the balance is paid when you do your final fitting. If you rather pay for it when you reserve it that is okay as well.</p>
                 </article>
                 <article>
                     <h3 class="text-2xl text-red-600 font-bold">What is included in my tuxedo or suit rental for prom?</h3>
-                    <p class="indent-3">The rental price includes 5 pieces:</p>
+                    <p class="indent-3 text-lg">The rental price includes 5 pieces:</p>
                     <ol class="list-decimal list-inside indent-6">
                         <li>Jacket</li>
                         <li>Pant</li>
@@ -33,32 +33,32 @@
                 </article>
                 <article>
                     <h3 class="text-2xl text-red-600 font-bold">Do you have shoes availble?</h3>
-                    <p class="indent-3">We do offer shoes for rent or purchase.</p>
+                    <p class="indent-3 text-lg">We do offer shoes for rent or purchase.</p>
                 </article>
                 <article>
                     <h3 class="text-2xl text-red-600 font-bold">How long can we keep it?</h3>
-                    <p class="indent-3">Typically if the Prom is on Saturday you will pick it up on Thursday or Friday and return it no later than Monday.</p>
+                    <p class="indent-3 text-lg">Typically if the Prom is on Saturday you will pick it up on Thursday or Friday and return it no later than Monday.</p>
                 </article>
                 <article>
                     <h3 class="text-2xl text-red-600 font-bold">Are the rentals slim fit and how do the pants generally fit?</h3>
-                    <p class="indent-3">Yes, all of our rentals and most all purchase tuxedos and suits are either slim or ultra slim. We also carry an athletic fit as well.</p>
+                    <p class="indent-3 text-lg">Yes, all of our rentals and most all purchase tuxedos and suits are either slim or ultra slim. We also carry an athletic fit as well.</p>
                 </article>
             </div>
         </section>
         <section>
-            <h2 class="text-xl lg:text-2xl font-bold py-2">Prom 2024 Pricing</h2>
+            <h2 class="text-2xl font-bold py-2">Prom 2025 Pricing</h2>
             <article>
-                <h3 class="text-lg">Prom Tuxedo and Suit Packages</h3>
-                <ul class="list-inside list-disc indent-5">
-                    <li>5 Piece: pants, shirt, vest, tie, and shoes --- <span class="font-bold">$140</span></li>
-                    <li>5 Piece: coat, pants, shirt, vest, and tie (no shoes included)  --- <span class="font-bold">$170</span></li>
-                    <li>6 Piece: coat, pants, shirt, vest, tie, and shoes --- <span class="font-bold">$185</span></li>
-                    <li>9 Piece: full 6 piece package, belt, cufflink set, pocket square, and socks --- <span class="font-bold">$215</span></li>
+                <h3 class="text-xl font-bold">Prom Tuxedo and Suit Packages</h3>
+                <ul class="list-inside list-disc">
+                    <li>5 Piece: Pants, shirt, vest, tie, and shoes --- <span class="font-bold">$140</span></li>
+                    <li>5 Piece: Coat, pants, shirt, vest, and tie (no shoes included)  --- <span class="font-bold">$170</span></li>
+                    <li>6 Piece: Coat, pants, shirt, vest, tie, and shoes --- <span class="font-bold">$185</span></li>
+                    <li>9 Piece: Full 6 piece package, belt, cufflink set, pocket square, and socks --- <span class="font-bold">$215</span></li>
                 </ul>
             </article>
             <article>
-                <h3 class="text-lg">Prom Individual Rental Items</h3>
-                <ul class="list-inside list-disc indent-5">
+                <h3 class="text-xl font-bold">Prom Individual Rental Items</h3>
+                <ul class="list-inside list-disc">
                     <li>Coat --- <span class="font-bold">$85</span></li>
                     <li>Pant --- <span class="font-bold">$45</span></li>
                     <li>Shirt --- <span class="font-bold">$25</span></li>
@@ -72,43 +72,64 @@
             </article>
         </section>
         <section class="">
-            <h2 class="text-xl lg:text-2xl font-bold py-2">Prom 2024 Dates</h2>
+            <h2 class="text-xl lg:text-2xl font-bold py-2">Prom 2025 Dates</h2>
             <article>
-                <h3>April 6th</h3>
+                <h3>March 29th</h3>
                 <ul class="list-inside indent-3">
-                    <li class="indent-6">Capital High School</li>
                     <li class="indent-6">Bishop Kelly High School</li>
                 </ul>
             </article>
             <article>
-                <h3>April 13th</h3>
+                <h3>April 5th</h3>
+                <ul class="list-inside indent-3">
+                    <li class="indent-6">Renaissance High School</li>
+                </ul>
+            </article>
+            <article>
+                <h3>April 12th</h3>
                 <ul class="list-inside indent-3">
                     <li class="indent-6">Rocky Mountain High School</li>
-                </ul>
-            </article>
-            <article>
-                <h3>April 20th</h3>
-                <ul class="list-inside indent-3">
-                    <li class="indent-6">Owyhee High School</li>
                     <li class="indent-6">Boise High School</li>
-                    <li class="indent-6">Borah High School</li>
-                    <li class="indent-6">Timberline High School</li>
-                    <li class="indent-6">Emmett High School</li>
-                    <li class="indent-6">Middleton High School</li>
+                    <li class="indent-6">Kuna High School</li>
                 </ul>
             </article>
             <article>
-                <h3>April 27th</h3>
+                <h3>April 19th</h3>
                 <ul class="list-inside indent-3">
-                    <li class="indent-6">Centennial High School</li>
-                    <li class="indent-6">Meridian High School</li>
-                    <li class="indent-6">Mountain View High School</li>
+                    <li class="indent-6">Timberline High School</li>
+                </ul>
+            </article>
+            <article>
+                <h3>April 26th</h3>
+                <ul class="list-inside indent-3">
+                    <li class="indent-6">Borah High School</li>
+                    <li class="indent-6">Eagle High School</li>
                 </ul>
             </article>         
             <article>
-                <h3>May 4th</h3>
+                <h3>May 3rd</h3>
                 <ul class="list-inside indent-3">
-                    <li class="indent-6">Eagle High School</li>
+                    <li class="indent-6">Owyhee High School</li>
+                    <li class="indent-6">Capital High School</li>
+                    <li class="indent-6">Mountain View High School</li>
+                </ul>
+            </article>
+            <article>
+                <h3>May 10th</h3>
+                <ul class="list-inside indent-3">
+                    <li class="indent-6">Centennial High School</li>
+                </ul>
+            </article>
+            <article>
+                <h3>May 17th</h3>
+                <ul class="list-inside indent-3">
+                    <li class="indent-6">Emmett High School</li>
+                </ul>
+            </article>
+            <article>
+                <h3>Unknown</h3>
+                <ul class="list-inside indent-3">
+                    <li class="indent-6">Meridian High School</li>
                 </ul>
             </article>
         </section>
@@ -125,18 +146,8 @@ const year = new Date().getFullYear();
 const headerProps = {
     title: `Prom ${year}`,
     content: `Prom ${year} season has arrived and Boise Tuxedo Shop is here to help. 
-    We understand that prom can be stressful, expensive, and time-consuming, but we are here to make it as easy as possible. 
-    Listed here are some FAQs that are related specifically to prom as well as the dates of proms in the area. 
-    If you have any other questions, feel free to contact us or view our general FAQs.`,
-    links: [
-        {
-            text: "Contact Us",
-            url: "/Contact"
-        },
-        {
-            text: "Freuqently Asked Questions",
-            url: "/Faq"
-        }]
+    We understand that prom can be stressful, expensive, and time-consuming, but we are here to make it as easy as possible.`,
+    links: []
 }
 
 const prom = {
