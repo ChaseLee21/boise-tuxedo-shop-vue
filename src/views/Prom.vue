bf9c34abd7fff25c03be995bffe39d3523b3c46e
Chase Seeberger <chase.seeberger@gmail.com>
2025-02-20 10:07:51 -0800

    Update prom tuxedo packages and FAQs for clarity and accuracy

--- src/views/Prom.vue
+++ src/views/Prom.vue
@@ -5,10 +5,9 @@
             <article>
                 <h3 class="text-xl font-bold">Prom Tuxedo and Suit Packages</h3>
                 <ul class="list-inside list-disc">
-                    <li>5 Piece: Pants, shirt, vest, tie, and shoes --- <span class="font-bold">$140</span></li>
-                    <li>5 Piece: Coat, pants, shirt, vest, and tie (no shoes included)  --- <span class="font-bold">$170</span></li>
-                    <li>6 Piece: Coat, pants, shirt, vest, tie, and shoes --- <span class="font-bold">$185</span></li>
-                    <li>9 Piece: Full 6 piece package, belt, cufflink set, pocket square, and socks --- <span class="font-bold">$215</span></li>
+                    <li>5 Piece: Coat, pants, shirt, vest, and tie (no shoes included)  --- <span class="font-bold">Starting at $149</span></li>
+                    <li>6 Piece: Coat, pants, shirt, vest, tie, and shoes --- <span class="font-bold">Starting at $165</span></li>
+                    <!-- <li>9 Piece: Full 6 piece package, belt, cufflink set, pocket square, and socks --- <span class="font-bold">$215</span></li> -->
                 </ul>
             </article>
             <!-- <article>
@@ -25,50 +24,6 @@
                     <li>Pocket Square --- <span class="font-bold">$5</span></li>
                 </ul>
             </article> -->
-        </section>
-        <section>
-            <h2 class="text-xl lg:text-2xl font-bold py-2">Prom FAQs</h2>
-            <div class="px-2">
-                <article>
-                    <h3 class="text-2xl text-red-600 font-bold">How soon do I need to reverse my tuxedo or suit?</h3>
-                    <p class="indent-3 text-lg">Ideally 2 to 3 weeks before prom, but we own all of our outfits and can always fit you even the week of prom.</p>
-                </article>
-                <article>
-                    <h3 class="text-2xl text-red-600 font-bold">Do you only rent?</h3>
-                    <p class="indent-3 text-lg">We do both rental and purchase, we carry over 3,000 tuxedos and suits in stock for rental and over 700 suits for purchase.</p>
-                </article>
-                <article>
-                    <h3 class="text-2xl text-red-600 font-bold">How much does it cost to rent a tuxedo or suit for prom?</h3>
-                    <p class="indent-3 text-lg">Please refer to our price chart below for a detailed overview of our prom 2024 pricing</p>
-                </article>
-                <article>
-                    <h3 class="text-2xl text-red-600 font-bold">When do I need to pay for my rental?</h3>
-                    <p class="indent-3 text-lg">We only require a 20.00 Deposit at the time you reserve and the balance is paid when you do your final fitting. If you rather pay for it when you reserve it that is okay as well.</p>
-                </article>
-                <article>
-                    <h3 class="text-2xl text-red-600 font-bold">What is included in my tuxedo or suit rental for prom?</h3>
-                    <p class="indent-3 text-lg">The rental price includes 5 pieces:</p>
-                    <ol class="list-decimal list-inside indent-6">
-                        <li>Jacket</li>
-                        <li>Pant</li>
-                        <li>Shirt</li>
-                        <li>Vest or Suspenders</li>
-                        <li>Bowtie or Necktie</li>
-                    </ol>
-                </article>
-                <article>
-                    <h3 class="text-2xl text-red-600 font-bold">Do you have shoes availble?</h3>
-                    <p class="indent-3 text-lg">We do offer shoes for rent or purchase.</p>
-                </article>
-                <article>
-                    <h3 class="text-2xl text-red-600 font-bold">How long can we keep it?</h3>
-                    <p class="indent-3 text-lg">Typically if the Prom is on Saturday you will pick it up on Thursday or Friday and return it no later than Monday.</p>
-                </article>
-                <article>
-                    <h3 class="text-2xl text-red-600 font-bold">Are the rentals slim fit and how do the pants generally fit?</h3>
-                    <p class="indent-3 text-lg">Yes, all of our rentals and most all purchase tuxedos and suits are either slim or ultra slim. We also carry an athletic fit as well.</p>
-                </article>
-            </div>
         </section>
         <section class="">
             <h2 class="text-xl lg:text-2xl font-bold py-2">Prom 2025 Dates</h2>
@@ -132,6 +87,43 @@
                 </ul>
             </article>
         </section>
+        <section>
+            <h2 class="text-xl lg:text-2xl font-bold py-2">Prom FAQs</h2>
+            <div class="px-2">
+                <article>
+                    <h3 class="text-2xl text-red-600 font-bold">How soon do I need to reserve my tuxedo or suit?</h3>
+                    <p class="indent-3 text-lg">Ideally 2 to 3 weeks before prom, but we own all of our outfits and can always fit you even the week of prom.</p>
+                </article>
+                <article>
+                    <h3 class="text-2xl text-red-600 font-bold">Do you only rent?</h3>
+                    <p class="indent-3 text-lg">We do both rental and purchase, we carry over 3,000 tuxedos and suits in stock for rental and over 700 suits for purchase.</p>
+                </article>
+                <!-- <article>
+                    <h3 class="text-2xl text-red-600 font-bold">What is included in my tuxedo or suit rental for prom?</h3>
+                    <p class="indent-3 text-lg">The rental price includes 5 pieces:</p>
+                    <ol class="list-decimal list-inside indent-6">
+                        <li>Jacket</li>
+                        <li>Pant</li>
+                        <li>Shirt</li>
+                        <li>Vest or Suspenders</li>
+                        <li>Bowtie or Necktie</li>
+                    </ol>
+                </article>
+                <article>
+                    <h3 class="text-2xl text-red-600 font-bold">Do you have shoes availble?</h3>
+                    <p class="indent-3 text-lg">We do offer shoes for rent or purchase.</p>
+                </article> -->
+                <article>
+                    <h3 class="text-2xl text-red-600 font-bold">How long can we keep it?</h3>
+                    <p class="indent-3 text-lg">Prom is on Saturday you will pick it up on Thursday or Friday and return it no later than Monday.</p>
+                </article>
+                <!-- <article>
+                    <h3 class="text-2xl text-red-600 font-bold">Are the rentals slim fit and how do the pants generally fit?</h3>
+                    <p class="indent-3 text-lg">Yes, all of our rentals and most all purchase tuxedos and suits are either slim or ultra slim. We also carry an athletic fit as well.</p>
+                </article> -->
+            </div>
+        </section>
+        
         <ImageGallery :title = prom.title :images = prom.images />
     </main>    
 </template>
