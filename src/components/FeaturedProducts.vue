90f1445368cbf222edef933da9f775606566d65d
Chase Seeberger <chase.seeberger@gmail.com>
2025-02-24 14:33:04 -0800

    Updating rent or buy component to match category component in styling

--- src/components/FeaturedProducts.vue
+++ src/components/FeaturedProducts.vue
@@ -1,22 +1,27 @@
 <template>
     <!-- Container  -->
-    <section class="my-2">
+    <section class="border-t-2 mb-4">
         <!-- Category Title -->
-        <header class="flex flex-row justify-start items-end mx-3">
+        <header class="flex flex-row justify-center items-end m-3">
             <h2 class="text-start text-3xl lg:text-4xl xl:text-5x font-bold mt-4">{{props.title}}</h2> 
-            <router-link class="text-xl text-black bg-white py-1 px-2 border-2 border-black bg-opacity-40 mx-4 hover:bg-black hover:bg-opacity-30 hover:border-opacity-30 hover:text-white hover:ease-in-out hover:transition-all hover:duration-300 duration-300" :to="{ name: 'Category', params: {category : props.route } }">View More</router-link>
+            <router-link class="text-xl text-black bg-white py-1 px-2 border-2 border-black bg-opacity-40 mx-4 hover:bg-black hover:bg-opacity-30 hover:border-opacity-30 hover:text-white hover:ease-in-out hover:transition-all hover:duration-300 duration-300" :to="{ name: 'Category', params: {category : props.route } }">View More {{props.title}}</router-link>
         </header>
         <!-- Product Container -->
-        <section class="sm:flex sm:flex-col md:h-full md:grid md:grid-flow-row md:grid-cols-2 md:w-[90%] lg:w-[80%] xl:grid-cols-4 md:mx-auto mb-8">
-            <article v-for="product in props.products" class="min-w-fit my-5 mx-1 rounded-md snap-center relative">
-                <router-link :to="{ name: 'Product', params: {id: product.id } }" class="hover:text-lg hover:ease-in-out hover:duration-500" >
-                    <figure class="relative text-white text-center overflow-hidden animate-image animate-button rounded h-full">
-                        <v-lazy-image width="480" height="800" :src="product.imageURL" :alt="product.imageAlt" class="min-w-full scale h-full object-cover zoom-image-transition" />
-                        <figcaption v-if="product.formattedName" class="absolute top-0 inset-x-0 w-full p-0.5 text-xl rounded-t bg-opacity-30 m-auto bg-black">{{ product.formattedName }}</figcaption>
-                        <figcaption v-else class="absolute top-0 inset-x-0 w-full p-0.5 text-xl rounded-t bg-opacity-40 m-auto bg-black">{{ product.name }}</figcaption>
-                        <button class="absolute bottom-6 right-1/2 translate-x-1/2 text-xl button-text py-1 px-2 border-2 button-transition bg-opacity-40 m-auto bg-black">View Details</button>
-                    </figure>
-                </router-link>
+        <section class="flex flex-wrap justify-center justify-items-center my-4">
+            <!-- Product Card -->
+            <article v-for="product in props.products" class="relative basis-80 mx-1">
+                <div class="grid grid-flow-row grid-rows-6">
+                    <!-- Clickable Area -->
+                    <router-link :to="{ name: 'Product', params: {id: product.id } }" class="hover:text-lg hover:ease-in-out hover:duration-500 row-span-5" >
+                        <!-- Product Image -->
+                        <figure class="relative text-white text-center overflow-hidden animate-image animate-button rounded-md">
+                            <v-lazy-image width="480" height="800" :src="product.imageURL" :alt="product.imageAlt" class="object-cover zoom-image-transition max-h-[800px] w-full aspect-[3/5]" />
+                            <button class="absolute bottom-6 right-1/2 translate-x-1/2 text-xl button-text py-1 px-2 border-2 button-transition bg-opacity-40 m-auto bg-black">View Details</button>
+                        </figure>
+                    </router-link>
+                    <!-- Product Title -->
+                    <p v-if="product.formattedName" class="flex-row text-center text-2xl m-1 row-span-1">{{ product.formattedName }}</p>
+                </div>
             </article>
         </section>
         <footer class="mx-auto text-center mb-4">
