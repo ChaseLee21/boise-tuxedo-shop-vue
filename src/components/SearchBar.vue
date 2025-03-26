ae3bc35fcb57aa65c44a663f6533b59f2993cb84
Chase Seeberger <chase.seeberger@gmail.com>
2025-03-26 13:31:06 -0700

    styling changes

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,8 +1,8 @@
 <template >
-    <section class="w-full flex justify-center bg-slate-800 py-2 ">
-        <input class="w-[66%] rounded p-2" type="search" v-model="searchQuery" placeholder="Search BTS">
+    <section class="w-full flex justify-center bg-slate-800 pt-2">
+        <input class="w-[66%] rounded rounded-e-none p-2" type="search" v-model="searchQuery" placeholder="Search Boise Tuxedo shop">
         <div class="animate-button">
-            <button class="p-1 mx-1 rounded text-white border-2 button-transition bg-black bg-opacity-40 text-xl">Search</button>
+            <button class="p-1 me-1 rounded rounded-s-none text-white border-2 border-s-0 button-transition bg-black bg-opacity-40 text-xl">Search</button>
         </div>
     </section>
 </template>
