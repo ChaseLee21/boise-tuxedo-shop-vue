3d3f49f413dd8699c92196537a195e995becbb2d
Chase Seeberger <chase.seeberger@gmail.com>
2025-03-31 13:17:55 -0700

    enhanced search functionality in SearchBar component to display results based on multiple keywords and added click handling for search results

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -2,14 +2,11 @@
     <section class="w-full flex-col items-center content-center justify-center bg-slate-800 pt-2">
         <div class="flex justify-center">
             <input class="w-[66%] rounded rounded-b-none p-2" type="search" v-model="searchQuery" v-on:input="onInputChange()" placeholder="Search Boise Tuxedo shop">
-            <!-- <div class="animate-button">
-                <button class="p-1 me-1 rounded rounded-s-none text-white border-2 border-s-0 button-transition bg-black bg-opacity-40 text-xl">Search</button>
-            </div> -->
         </div>
-        <div class="fixed z-50 w-full">
+        <div v-if="showSearchResults" class="fixed z-50 w-full">
             <ul class="flex-col w-[66%] shadow-md border justify-start m-auto bg-white p-2 rounded rounded-t-none">
-                <div v-for="product in results">
-                    <li>
+                <div  v-for="product in results">
+                    <li @click="handleSearchResultsClick(product)">
                         <router-link :to="{ name: 'Product', params: {id: product.id } }">
                             {{product.name}}
                         </router-link>
@@ -26,28 +23,52 @@ import { RouterLink } from 'vue-router';
 
 let searchQuery = ref("")
 let products = ref([]);
+const showSearchResults = ref(false);
 
 onMounted(async () => {
     products.value = await getProducts();
 })
 
+// Search Results
+const results = computed(() => {
+    showSearchResults.value = true
+    let resultsArr; 
+    let query = searchQuery.value.toLowerCase().trim();
+    let keywords = query.split(" ");
+    if (query.length < 1) return null;
+    resultsArr = products.value.filter(product => {
+        return containsKeyword(product.name,keywords);
+    });
+    while (resultsArr.length > 8) {
+        resultsArr.pop();
+    }
+
+    return resultsArr;
+})
+
+function containsKeyword(a, keywords) {
+    a = a.toLowerCase().trim();
+    for (let keyword of keywords) {
+        console.log(keyword, a);
+        if (!a.includes(keyword)) {
+            return false;
+        }
+    }
+    return true;
+}
+
 function onInputChange() {
     console.log("results", results.value)
 }
 
-const results = computed(() => {
-    if (searchQuery.value.length < 1) return null
-    let arr = products.value.filter(product => product.name.toLowerCase().trim().includes(searchQuery.value.toLowerCase().trim()));
-    while (arr.length > 5) {
-        arr.pop();
-    }
-    return arr;
-})
-
+function handleSearchResultsClick(product) {
+    searchQuery.value = product.name;
+    showSearchResults.value = false;
+}
 </script>
 
+<!-- Styling -->
 <style scoped>
-
 .button-transition {
     transition: background .3s ease-in-out, color .3s ease-in-out;
 }
@@ -56,6 +77,4 @@ const results = computed(() => {
     background: #FFFFFFcc;
     color: #000;
 }
-
-
 </style>
