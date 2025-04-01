11dc7e02db3e871085f16addc39a3e0d40261706
Chase Seeberger <chase.seeberger@gmail.com>
2025-04-01 10:03:32 -0700

    fixed bug where mobile navbar dropdown menu was below the news banner on the z level

--- src/components/Navigation Bar/NavBarMobile.vue
+++ src/components/Navigation Bar/NavBarMobile.vue
@@ -15,7 +15,7 @@
             </button>
             <router-link to="/" class="text-2xl font-bold">Boise Tuxedo Shop</router-link>
             <Transition>
-                <nav v-show="navOpen" class="absolute left-0 top-12 w-full">
+                <nav v-show="navOpen" class="absolute left-0 top-12 w-full z-50">
                     <ul class="flex flex-col p-3 text-lg bg-gray-800 text-white">
                         <li class="p-2 font-bold hover:text-zinc-300"><router-link to="/">Home</router-link></li>
                         <li class="p-2 pb-0 hover:text-zinc-300"><router-link to="/RentOrBuy">Rent or Buy</router-link></li>
