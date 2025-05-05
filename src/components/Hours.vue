9a660320ee208712e12c3d521425351bee18525a
Chase Seeberger <chase.seeberger@gmail.com>
2025-05-05 15:38:26 -0700

    Updated hours of operation

--- src/components/Hours.vue
+++ src/components/Hours.vue
@@ -2,7 +2,7 @@
     <section>
         <ul class="col-span-1 p-2">
             <li><p class="font-bold">Hours</p></li>
-            <li>Mon – Fri: 10am – 7pm</li>
+            <li>Mon – Fri: 10am – 6pm</li>
             <li>Saturday: 10am – 5pm</li>
             <li>Sunday: Closed</li>
         </ul>
