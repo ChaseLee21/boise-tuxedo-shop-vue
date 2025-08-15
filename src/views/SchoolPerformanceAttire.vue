f5850ccbf1b86f73c34b87e15f0987491cf1a880
Chase Seeberger <chase.seeberger@gmail.com>
2025-08-15 15:56:12 -0700

    Updated the information on the SchoolPerformanceAttire component to match that of the flyer

--- src/views/SchoolPerformanceAttire.vue
+++ src/views/SchoolPerformanceAttire.vue
@@ -2,21 +2,21 @@
     <Header :title = headerProps.title :content = headerProps.content :links = headerProps.links />
     <main class="xl:w-[80vw] xl:flex xl:flex-col xl:m-auto mx-2">
         <section>
-            <h2 class="text-xl lg:text-2xl font-bold py-2">Rental Packages</h2>
+            <h2 class="text-xl lg:text-2xl font-bold py-2">2025 - 2026 Rental Packages</h2>
             <div class="px-2">
                 <article>
-                    <p class="indent-3 text-xl">6 Piece (Coat, Pants, Shirt, Vest, Tie and Shoes) --- <span class="font-bold">$185</span> normal price</p>
-                    <p class="indent-10 text-xl">- Special Pricing for Marine Corps Ball Only --- <span class="text-red-600 font-bold">$145</span> event only price</p>
+                    <p class="indent-3 text-xl"></p>
+                    <p class="indent-3 text-xl">We would like to extend this offer to the high school music groups yet another year. Adding a new program to the mix.</p>
                 </article>
                 <br></br>
                 <article>
-                    <p class="indent-3 text-xl">9 Piece (Full 6 piece package PLUS Pocket Square, Socks, and your choice of Belt or Cufflink/Stud Set) --- <span class="font-bold">$209 - $219</span> normal price</p>
-                    <p class="indent-10 text-xl">- Special Pricing for Marine Corps Ball Only --- <span class="text-red-600 font-bold">$189</span> event only price</p>
+                    <p class="indent-3 text-xl">Program A: Renting for the year, better short term cost, --- <span class="font-bold">$109.95</span> * Rental price includes alterations</p>
+                    <p class="indent-3 text-xl">Program B: Purchasing, better long term cost --- <span class="font-bold">$149.95</span> * There will be a minor additional fee for alterations</p>
                 </article>
                 <br></br>
+                <br></br>
                 <article>
-                    <p class="indent-3 text-xl">* If you are interested in <span class="font-bold">purchasing</span> a suit or tuxedo for the event you can use the same coupon code USMC250 to take up to 20% off your total purchase. *</p>
-                    <p class="indent-3 text-xl">* <span class="font-bold">Tailoring/alteration</span> prices for purchase suits are not included in the discounted price and will be charged separately. *</p>
+                    <p class="indent-3 text-xl">* Both programs offer our <span class="font-bold">trade in option</span>. If at anytime you need to trade in a coat, pant or shirt for a different size we only charge a cleaning fee, opposed to buying or renting the whole tuxedo or suit.</p>
                 </article>
             </div>
         </section>
@@ -24,20 +24,20 @@
             <h2 class="text-xl lg:text-2xl font-bold py-2">Frequently Asked Questions</h2>
             <div class="px-2">
                 <article>
-                    <h3 class="text-2xl text-red-600 font-bold">How do I receive the discount?</h3>
-                    <p class="indent-3 text-xl">When renting your tuxedo or suit for the Marine Corp Ball, simply mention the event and use code "USMC250" in store to receive your discount!</p>
+                    <h3 class="text-2xl text-red-600 font-bold">Who is eligible for this offer?</h3>
+                    <p class="indent-3 text-xl">If you are a high school or middle school student participating in a school performance, you are eligible for this offer. If you have any questions about your eligibility, please feel free to contact us. <a class="link text-md" href="tel:208-375-9616">(208) 375-9616</a></p>
                 </article>
                 <article>
-                    <h3 class="text-2xl text-red-600 font-bold">Does the discount only apply to one style?</h3>
-                    <p class="indent-3 text-xl">No, the discount applies to all styles available for rent.</p>
+                    <h3 class="text-2xl text-red-600 font-bold">How does the trade in option work?</h3>
+                    <p class="indent-3 text-xl">If at any time you need to trade in a coat, pant, or shirt for a different size, we only charge a cleaning fee, opposed to buying or renting the whole tuxedo or suit. This option is available regardless if you purchased or rented your attire from us.</p>
                 </article>
                 <article>
-                    <h3 class="text-2xl text-red-600 font-bold">What is the total price of the rental after the discount?</h3>
-                    <p class="indent-3 text-xl">Pricing varies based on the style you choose to rent, but all for rent styles are eligible for the discount.</p>
+                    <h3 class="text-2xl text-red-600 font-bold">What sizing options are available?</h3>
+                    <p class="indent-3 text-xl">We offer a wide range of sizes for our tuxedos and suits, including slim fit and regular fit options. If you have specific sizing needs, please contact us for assistance and we will be more than happy to assist you.</p>
                 </article>
                 <article>
-                    <h3 class="text-2xl text-red-600 font-bold">Question not answered here?</h3>
-                    <p class="indent-3 text-xl"><a class="link" href="tel:208-986-2999">Text or Call: (208) 986-2999</a></p>
+                    <h3 class="text-2xl text-red-600 font-bold">Do I need to come in to get fitted?</h3>
+                    <p class="indent-3 text-xl">We recommend coming in for a fitting to ensure the best possible fit for your tuxedo or suit. Our experienced staff will be able to assist you in finding the perfect size and style for your needs.</p>
                 </article>
             </div>
         </section>
@@ -51,16 +51,16 @@ import Header from '../components/Header.vue';
 
 const headerProps = {
     title: `School Performance Attire`,
-    content: `Boise Tuxedo Shop is a family-owned and operated formalwear provider in Meridian, proudly supporting our military. We're honored to partner with the Marine Corps for the 250th anniversary Ball—suit up locally with pride and tradition. Use code "USMC250" in store to receive your discount!`,
+    content: `The Boise Tuxedo Shop & our parent company Tuxgear Inc. have been serving high schools all over the treasure valley with our tuxedos, suits, and huge range of accessories for the past 26 years. Our team loves being able to help the new & returning students each year to get fitted into a suit or tuxedo. It is awesome watching each student as they exit the fitting area smiling glowing with confidence.`,
     links: []
 }
 
 const images = {
-    title: "Marine Corp Ball Tuxedo Suit Discount",
+    title: "School Performance Attire Tuxedo and Suit Package",
     images: [
         {
-            url: "https://btsimagestorage.blob.core.windows.net/product-images/Kenneth%20Cole%20Slim%20Black%20Tuxedo%20with%20Satin%20Trim%20Lapel%20%28Rocco%29.webp",
-            alt: "Marine Corp Ball Tuxedo Suit Discount"
+            url: "https://btsimagestorage.blob.core.windows.net/product-images/Belk.webp",
+            alt: "School Performance Attire Tuxedo and Suit Package"
         }
     ]
 } 
